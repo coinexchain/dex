2306f3d394f18bceab6cac28d633aa9425827b04
jjz <woaf1003@gmail.com>
2019-05-14 17:02:01 +0800

    add check.sh

---
+++ check.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+
+set -ex
+
+if [ ! -x "$(type -p glide)" ]; then
+    exit 1
+fi
+
+if [ ! -x "$(type -p gometalinter)" ]; then
+    exit 1
+fi
+
+
+
+find . -name "*.go" -not -path "./vendor/*" -not -path "./git/*" | xargs gofmt -w
+
+linter_targets=$(glide novendor)
+
+test -z "$(gometalinter -j 4 --disable-all \
+--enable=gofmt \
+--enable=golint \
+--enable=gosimple \
+--enable=ineffassign \
+--enable=vet \
+--enable=misspell \
+--enable=unconvert \
+--exclude='should have comment' \
+--exclude='and that stutters;' \
+--deadline=10m $linter_targets 2>&1 | grep -v 'ALL_CAPS\|OP_' 2>&1 | tee /dev/stderr)"
+
+go test  -covermode=atomic -coverprofile=coverage.out -race -tags rpctest $linter_targets
+
+
+
