c46a5851b6311e6cccce54590257d48d529867f4
zxh0 <zhangxiuhong2013@gmail.com>
2019-05-15 17:58:22 +0800

    create GenesisState and TokenKeeper for asset module

---
+++ x/asset/genesis.go
@@ -0,0 +1,40 @@
+package asset
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// GenesisState - all asset state that must be provided at genesis
+type GenesisState struct {
+	Params Params `json:"params"`
+}
+
+// NewGenesisState - Create a new genesis state
+func NewGenesisState(params Params) GenesisState {
+	return GenesisState{
+		Params: params,
+	}
+}
+
+// DefaultGenesisState - Return a default genesis state
+func DefaultGenesisState() GenesisState {
+	return NewGenesisState(DefaultParams())
+}
+
+// InitGenesis - Init store state from genesis data
+func InitGenesis(ctx sdk.Context, data GenesisState) {
+	// TODO
+}
+
+// ExportGenesis returns a GenesisState for a given context and keeper
+func ExportGenesis(ctx sdk.Context, tk TokenKeeper) GenesisState {
+	params := tk.GetParams(ctx)
+	return NewGenesisState(params)
+}
+
+// ValidateGenesis performs basic validation of auth genesis data returning an
+// error for any failed validation criteria.
+func ValidateGenesis(data GenesisState) error {
+	// TODO
+	return nil
+}
