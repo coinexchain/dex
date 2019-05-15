d25bbe35b52ac2ca5c5af89e0baf172aa1e1b407
ZC.Fan <fanzc@gmail.com>
2019-05-15 20:17:19 +0800

    Add asset params ParamKeyTable

--- x/asset/params.go
+++ x/asset/params.go
@@ -1,11 +1,36 @@
 package asset
 
 import (
+	"bytes"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/params"
 )
+
+// DefaultParamspace defines the default asset module parameter subspace
+const (
+	DefaultParamspace = "asset"
+)
+
+var ()
+
+// Parameter keys
+var (
+	KeyIssueTokenFee                 = []byte("IssueTokenFee")
+	KeyFreezeAddrFee                 = []byte("FreezeAddrFee")
+	KeyUnFreezeAddrFee               = []byte("UnFreezeAddrFee")
+	KeyFreezeTokenFee                = []byte("FreezeTokenFee")
+	KeyUnFreezeTokenFee              = []byte("UnFreezeTokenFee")
+	KeyTokenFreezeWhitelistAddFee    = []byte("TokenFreezeWhitelistAddFee")
+	KeyTokenFreezeWhitelistRemoveFee = []byte("TokenFreezeWhitelistRemoveFee")
+	KeyBurnFee                       = []byte("BurnFee")
+	KeyMintFee                       = []byte("MintFee")
+)
+
+var _ params.ParamSet = &Params{}
 
 // Params defines the parameters for the asset module.
 type Params struct {
+	// FeeParams define the rules according to which fee are charged.
 	IssueTokenFee                 sdk.Coins `json:"issue_token_fee"`
 	FreezeAddrFee                 sdk.Coins `json:"freeze_address_fee"`
 	UnFreezeAddrFee               sdk.Coins `json:"unfreeze_address_fee"`
@@ -17,8 +42,39 @@ type Params struct {
 	MintFee                       sdk.Coins `json:"mint_fee"`
 }
 
+// ParamKeyTable for asset module
+func ParamKeyTable() params.KeyTable {
+	return params.NewKeyTable().RegisterParamSet(&Params{})
+}
+
+// ParamSetPairs implements the ParamSet interface and returns all the key/value pairs
+// pairs of asset module's parameters.
+// nolint
+func (p *Params) ParamSetPairs() params.ParamSetPairs {
+	return params.ParamSetPairs{
+
+		{KeyIssueTokenFee, &p.IssueTokenFee},
+		{KeyFreezeAddrFee, &p.FreezeAddrFee},
+		{KeyUnFreezeAddrFee, &p.UnFreezeAddrFee},
+		{KeyFreezeTokenFee, &p.FreezeTokenFee},
+		{KeyUnFreezeTokenFee, &p.UnFreezeTokenFee},
+		{KeyTokenFreezeWhitelistAddFee, &p.TokenFreezeWhitelistAddFee},
+		{KeyTokenFreezeWhitelistRemoveFee, &p.TokenFreezeWhitelistRemoveFee},
+		{KeyBurnFee, &p.BurnFee},
+		{KeyMintFee, &p.MintFee},
+	}
+}
+
+// Equal returns a boolean determining if two Params types are identical.
+func (p Params) Equal(p2 Params) bool {
+	bz1 := msgCdc.MustMarshalBinaryLengthPrefixed(&p)
+	bz2 := msgCdc.MustMarshalBinaryLengthPrefixed(&p2)
+	return bytes.Equal(bz1, bz2)
+}
+
 // DefaultParams returns a default set of parameters.
 func DefaultParams() Params {
+
 	return Params{
 		IssueTokenFee:                 cet(10000),
 		FreezeAddrFee:                 cet(10),
