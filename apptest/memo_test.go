f694d0d80e073d9aca6ce18888e09a5038fc8d10
zxh0 <zhangxiuhong2013@gmail.com>
2019-05-16 14:44:18 +0800

    Update memo_test.go

--- apptest/memo_test.go
+++ apptest/memo_test.go
@@ -7,7 +7,6 @@ import (
 	dbm "github.com/tendermint/tendermint/libs/db"
 	"github.com/tendermint/tendermint/libs/log"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/bank"
@@ -16,19 +15,12 @@ import (
 	"github.com/coinexchain/dex/denoms"
 )
 
-func _TestMemo(t *testing.T) {
-	anteOpt := func(bapp *baseapp.BaseApp) {
-		bapp.SetAnteHandler(func(ctx sdk.Context, tx sdk.Tx, simulate bool) (newCtx sdk.Context, res sdk.Result, abort bool) {
-			newCtx = ctx.WithBlockGasMeter(sdk.NewGasMeter(100))
-			return
-		})
-	}
-
+func TestMemo(t *testing.T) {
 	logger := log.NewNopLogger()
 	db := dbm.NewMemDB()
-	app := app.NewCetChainApp(logger, db, nil, false, 10000, anteOpt)
-	app.SetInitChainer(nil)
-	app.InitChain(abci.RequestInitChain{})
+	app := app.NewCetChainApp(logger, db, nil, true, 10000)
+
+	app.InitChain(abci.RequestInitChain{AppStateBytes:[]byte("{}")})
 
 	header := abci.Header{Height: 1}
 	app.BeginBlock(abci.RequestBeginBlock{Header: header})
