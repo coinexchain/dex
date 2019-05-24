9bcf797dbb1293c287643e7555eee4b4bc8c1375
zxh0 <zhangxiuhong2013@gmail.com>
2019-05-24 16:24:59 +0800

    Fix typo

--- modules/authx/errors.go
+++ modules/authx/errors.go
@@ -5,12 +5,12 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 const (
 	CodeSpaceAuthX sdk.CodespaceType = "authx"
 
-	CodeAccoutxInvalid     sdk.CodeType = 200
+	CodeAccountxInvalid    sdk.CodeType = 200
 	CodeInvalidMinGasPrice sdk.CodeType = 201
 )
 
-func ErrInvalidAccoutx(msg string) sdk.Error {
-	return sdk.NewError(CodeSpaceAuthX, CodeAccoutxInvalid, msg)
+func ErrInvalidAccountx(msg string) sdk.Error {
+	return sdk.NewError(CodeSpaceAuthX, CodeAccountxInvalid, msg)
 }
 
 func ErrInvalidMinGasPrice(msg string) sdk.Error {
