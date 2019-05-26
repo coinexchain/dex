48c7a443df8ca2ac7e7b9f605204d9b6b4155327
ZC.Fan <fanzc912@gmail.com>
2019-05-26 15:58:55 +0800

    Add UT for asset query

---
+++ modules/asset/query_test.go
@@ -0,0 +1,81 @@
+package asset
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+func Test_queryToken(t *testing.T) {
+	input := setupTestInput()
+	req := abci.RequestQuery{
+		Path: fmt.Sprintf("custom/%s/%s", RouterKey, QueryToken),
+		Data: []byte{},
+	}
+
+	// no token
+	res, err := queryToken(input.ctx, req, input.tk)
+	require.Error(t, err)
+	require.Nil(t, res)
+
+	token, err := NewToken("ABC Token", "abc", 2100, tAccAddr,
+		false, false, false, false)
+	require.NoError(t, err)
+	err = input.tk.SetToken(input.ctx, token)
+	require.NoError(t, err)
+
+	req.Data = input.cdc.MustMarshalJSON(NewQueryAssetParams(""))
+	res, err = queryToken(input.ctx, req, input.tk)
+	require.Error(t, err)
+	require.Nil(t, res)
+
+	req.Data = input.cdc.MustMarshalJSON(NewQueryAssetParams("www"))
+	res, err = queryToken(input.ctx, req, input.tk)
+	require.Error(t, err)
+	require.Nil(t, res)
+
+	req.Data = input.cdc.MustMarshalJSON(NewQueryAssetParams("abc"))
+	res, err = queryToken(input.ctx, req, input.tk)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	var resToken Token
+	input.cdc.MustUnmarshalJSON(res, &resToken)
+	require.Equal(t, "abc", resToken.GetSymbol())
+
+}
+
+func Test_queryAllTokenList(t *testing.T) {
+	input := setupTestInput()
+	req := abci.RequestQuery{
+		Path: fmt.Sprintf("custom/%s/%s", RouterKey, QueryTokenList),
+		Data: []byte{},
+	}
+
+	res, err := queryAllTokenList(input.ctx, req, input.tk)
+	require.Error(t, err)
+	require.Nil(t, res)
+
+	token1, err := NewToken("ABC Token", "abc", 2100, tAccAddr,
+		false, false, false, false)
+	require.NoError(t, err)
+	err = input.tk.SetToken(input.ctx, token1)
+	require.NoError(t, err)
+
+	token2, err := NewToken("XYZ Token", "xyz", 2100, tAccAddr, false, false, false, false)
+	require.NoError(t, err)
+	err = input.tk.SetToken(input.ctx, token2)
+	require.NoError(t, err)
+
+	res, err = queryAllTokenList(input.ctx, req, input.tk)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	var tokens []Token
+	input.cdc.MustUnmarshalJSON(res, &tokens)
+	require.Equal(t, 2, len(tokens))
+	require.Contains(t, []string{"abc", "xyz"}, tokens[0].GetSymbol())
+	require.Contains(t, []string{"abc", "xyz"}, tokens[1].GetSymbol())
+}
---
+++ modules/asset/test_util.go
@@ -0,0 +1,77 @@
+package asset
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/tendermint/tendermint/libs/log"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+	dbm "github.com/tendermint/tendermint/libs/db"
+)
+
+var _, _, tAccAddr = keyPubAddr()
+
+type testInput struct {
+	cdc     *codec.Codec
+	ctx     sdk.Context
+	tk      TokenKeeper
+	handler sdk.Handler
+}
+
+func setupTestInput() testInput {
+	db := dbm.NewMemDB()
+
+	cdc := codec.New()
+	RegisterCodec(cdc)
+	auth.RegisterBaseAccount(cdc)
+
+	assetCapKey := sdk.NewKVStoreKey(StoreKey)
+	authCapKey := sdk.NewKVStoreKey(auth.StoreKey)
+	fckCapKey := sdk.NewKVStoreKey(auth.FeeStoreKey)
+	keyParams := sdk.NewKVStoreKey(params.StoreKey)
+	tkeyParams := sdk.NewTransientStoreKey(params.TStoreKey)
+
+	ms := store.NewCommitMultiStore(db)
+	ms.MountStoreWithDB(assetCapKey, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(authCapKey, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(fckCapKey, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(keyParams, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(tkeyParams, sdk.StoreTypeTransient, db)
+
+	ms.LoadLatestVersion()
+
+	ak := auth.NewAccountKeeper(
+		cdc,
+		authCapKey,
+		params.NewKeeper(cdc, keyParams, tkeyParams).Subspace(auth.DefaultParamspace),
+		auth.ProtoBaseAccount,
+	)
+	fck := auth.NewFeeCollectionKeeper(
+		cdc,
+		fckCapKey,
+	)
+	tk := NewKeeper(
+		cdc,
+		assetCapKey,
+		params.NewKeeper(cdc, keyParams, tkeyParams).Subspace(DefaultParamspace),
+		ak,
+		fck,
+	)
+
+	ctx := sdk.NewContext(ms, abci.Header{ChainID: "test-chain-id"}, false, log.NewNopLogger())
+	handler := NewHandler(tk)
+
+	return testInput{cdc, ctx, tk, handler}
+}
+
+func keyPubAddr() (crypto.PrivKey, crypto.PubKey, sdk.AccAddress) {
+	key := secp256k1.GenPrivKey()
+	pub := key.PubKey()
+	addr := sdk.AccAddress(pub.Address())
+	return key, pub, addr
+}
