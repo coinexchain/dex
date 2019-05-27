8c41e8ba28659b2a3a81adbbd47f937b4eaa223d
Hongmin Wang <whunmr@gmail.com>
2019-05-27 19:04:48 +0800

    Change default key name in testing node configuration

--- scripts/setup_single_testing_node.sh
+++ scripts/setup_single_testing_node.sh
@@ -14,8 +14,8 @@ fi
 
 rm -rdf ~/.cetd ~/.cetcli
 ./cetd init moniker0 --chain-id=coinexdex
-./cetcli keys add validator0 <<<$'12345678\n12345678\n'
-./cetd add-genesis-account $(./cetcli keys show validator0 -a) 10000000000000000cet
-./cetd gentx --name validator0 <<<$'12345678\n12345678\n'
+./cetcli keys add bob <<<$'12345678\n12345678\n'
+./cetd add-genesis-account $(./cetcli keys show bob -a) 10000000000000000cet
+./cetd gentx --name bob <<<$'12345678\n12345678\n'
 ./cetd collect-gentxs
 
