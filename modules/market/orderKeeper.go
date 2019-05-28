01d189f687c3fd36a55d9eb76e1c5c792c53c93d
wangkui0508 <wangkui0508@hotmail.com>
2019-05-28 20:19:04 +0800

    Add the OrderKeeper interface and its implmentation, which manages the KVStore of orders

--- modules/market/orderKeeper.go
+++ modules/market/orderKeeper.go
@@ -1,18 +1,298 @@
 package market
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/coinexchain/dex/modules/market/match"
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+//nolint
+var (
+	OrderBookKeyPrefix      = []byte{0x11}
+	BidListKeyPrefix        = []byte{0x12}
+	BidListKeyPrefixPlusOne = []byte{0x13}
+	AskListKeyPrefix        = []byte{0x13}
+	AskListKeyPrefixPlusOne = []byte{0x14}
+	OrderQueueKeyPrefix     = []byte{0x14}
+	OrderQueueKeyPrefixPlusOne     = []byte{0x15}
+	IocListKeyPrefix        = []byte{0x15}
+)
+
+const (
+	DecByteCount = 40
+	GTE          = 3
+	IOC          = 4
+	Buy          = match.BUY
+	Sell         = match.SELL
+	LIMIT        = 2
+)
+
 type OrderKeeper interface {
-	Add(order *Order) error
-	Exists(order *Order) bool
-	Remove(order *Order) error
-	GetMatchingCandidates() []match.OrderForTrade
+	Add(order *Order) sdk.Error
+	Exists(orderID string) bool
+	Remove(order *Order) sdk.Error
 	GetOlderThan(height int64) []*Order
+	GetOrdersAtHeight(height int64) []*Order
+	QueryOrder(orderID string) *Order
+	GetOrdersFromUser(user string) []string
+	GetMatchingCandidates() []*Order
 }
 
-func NewOrderKeeper(store *sdk.KVStore, symbol string) *OrderKeeper {
+type PersistentOrderKeeper struct {
+	store  sdk.KVStore
+	symbol string
+	codec  *codec.Codec
+}
+
+func concatCopyPreAllocate(slices [][]byte) []byte {
+	var totalLen int
+	for _, s := range slices {
+		totalLen += len(s)
+	}
+	tmp := make([]byte, totalLen)
+	var i int
+	for _, s := range slices {
+		i += copy(tmp[i:], s)
+	}
+	return tmp
+}
+
+func (keeper *PersistentOrderKeeper) orderBookKey(orderID string) []byte {
+	return concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		OrderBookKeyPrefix,
+		[]byte(orderID),
+	})
+}
+
+func (keeper *PersistentOrderKeeper) bidListKey(order *Order) []byte {
+	return concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		BidListKeyPrefix,
+		decToBigEndianBytes(order.Price),
+		[]byte(order.OrderID()),
+	})
+}
+
+func (keeper *PersistentOrderKeeper) askListKey(order *Order) []byte {
+	return concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		AskListKeyPrefix,
+		decToBigEndianBytes(order.Price),
+		[]byte(order.OrderID()),
+	})
+}
+
+func (keeper *PersistentOrderKeeper) orderQueueKey(order *Order) []byte {
+	return concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		OrderQueueKeyPrefix,
+		int64ToBigEndianBytes(order.Height),
+		[]byte(order.OrderID()),
+	})
+}
+
+func NewOrderKeeper(store sdk.KVStore, symbol string, codec *codec.Codec) OrderKeeper {
+	return &PersistentOrderKeeper{
+		store:  store,
+		symbol: symbol,
+		codec:  codec,
+	}
+}
+
+func decToBigEndianBytes(d sdk.Dec) []byte {
+	var result [DecByteCount]byte
+	bytes := d.Int.Bytes()
+	for i := 1; i <= len(bytes); i++ {
+		result[DecByteCount-i] = bytes[len(bytes)-i]
+	}
+	return result[:]
+}
+
+func int64ToBigEndianBytes(n int64) []byte {
+	var result [8]byte
+	for i := 0; i < 8; i++ {
+		result[i] = byte(n >> (8 * uint(i)))
+	}
+	return result[:]
+}
+
+func (keeper *PersistentOrderKeeper) Add(order *Order) sdk.Error {
+	key := keeper.orderBookKey(order.OrderID())
+	value := keeper.codec.MustMarshalBinaryBare(order)
+	keeper.store.Set(key, value)
+
+	if order.TimeInForce == GTE {
+		key = keeper.orderQueueKey(order)
+		keeper.store.Set(key, nil)
+	}
+	if order.Side == match.BID {
+		key = keeper.bidListKey(order)
+		keeper.store.Set(key, nil)
+	}
+	if order.Side == match.ASK {
+		key = keeper.askListKey(order)
+		keeper.store.Set(key, nil)
+	}
 	return nil
+}
+
+func (keeper *PersistentOrderKeeper) Exists(orderID string) bool {
+	key := keeper.orderBookKey(orderID)
+	return keeper.store.Has(key)
+}
+
+func (keeper *PersistentOrderKeeper) Remove(order *Order) sdk.Error {
+	if !keeper.Exists(order.OrderID()) {
+		return ErrNoExistKeyInStore()
+	}
+	key := keeper.orderBookKey(order.OrderID())
+	keeper.store.Delete(key)
+
+	if order.TimeInForce == GTE {
+		key = keeper.orderQueueKey(order)
+		keeper.store.Delete(key)
+	}
+	if order.Side == match.BID {
+		key = keeper.bidListKey(order)
+		keeper.store.Delete(key)
+	}
+	if order.Side == match.ASK {
+		key = keeper.askListKey(order)
+		keeper.store.Delete(key)
+	}
+	return nil
+}
+
+func (keeper *PersistentOrderKeeper) GetOlderThan(height int64) []*Order {
+	var result []*Order
+	start := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		OrderQueueKeyPrefix,
+	})
+	end := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		OrderQueueKeyPrefix,
+		int64ToBigEndianBytes(height),
+	})
+	for iter := keeper.store.ReverseIterator(start, end); iter.Valid(); iter.Next() {
+		ikey := iter.Key()
+		orderID := string(ikey[len(end):])
+		result = append(result, keeper.QueryOrder(orderID))
+	}
+	return result
+}
+
+func (keeper *PersistentOrderKeeper) GetOrdersAtHeight(height int64) []*Order {
+	var result []*Order
+	start := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		OrderQueueKeyPrefix,
+		int64ToBigEndianBytes(height),
+	})
+	end := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		OrderQueueKeyPrefix,
+		int64ToBigEndianBytes(height + 1),
+	})
+	for iter := keeper.store.Iterator(start, end); iter.Valid(); iter.Next() {
+		ikey := iter.Key()
+		orderID := string(ikey[len(end):])
+		result = append(result, keeper.QueryOrder(orderID))
+	}
+	return result
+}
+
+func (keeper *PersistentOrderKeeper) QueryOrder(orderID string) *Order {
+	key := keeper.orderBookKey(orderID)
+	orderBytes := keeper.store.Get(key)
+	order := &Order{}
+	keeper.codec.MustUnmarshalBinaryBare(orderBytes, order)
+	return order
+}
+
+func (keeper *PersistentOrderKeeper) GetOrdersFromUser(user string) []string {
+	key := keeper.orderBookKey(user + "-")
+	nextKey := keeper.orderBookKey(user + string([]byte{0xFF}))
+	startPos := len(key) - len(user) - 1
+	var result []string
+	for iter := keeper.store.ReverseIterator(key, nextKey); iter.Valid(); iter.Next() {
+		k := iter.Key()
+		result = append(result, string(k[startPos:]))
+	}
+	return result
+}
+
+func (keeper *PersistentOrderKeeper) GetMatchingCandidates() []*Order {
+	priceStartPos := len(keeper.symbol) + 2
+	priceEndPos := priceStartPos + DecByteCount
+	bidListStart := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		BidListKeyPrefix,
+	})
+	bidListEnd := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		BidListKeyPrefixPlusOne,
+	})
+	askListStart := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		AskListKeyPrefix,
+	})
+	askListEnd := concatCopyPreAllocate([][]byte{
+		[]byte(keeper.symbol),
+		[]byte{0x0},
+		AskListKeyPrefixPlusOne,
+	})
+	bidIter := keeper.store.ReverseIterator(bidListStart, bidListEnd)
+	askIter := keeper.store.Iterator(askListStart, askListEnd)
+	if !bidIter.Valid() || !askIter.Valid() {
+		return nil
+	}
+	firstBidKey := bidIter.Key()
+	firstAskKey := askIter.Key()
+	firstBidPrice := firstBidKey[priceStartPos:priceEndPos]
+	firstAskPrice := firstAskKey[priceStartPos:priceEndPos]
+	if bytes.Compare(firstAskPrice, firstBidPrice) > 0 {
+		return nil
+	}
+	orderIDList := []string{string(firstBidKey[priceEndPos:]), string(firstAskKey[priceEndPos:])}
+	for _, s := range orderIDList {
+		fmt.Printf("here! %s\n",s)
+	}
+	for askIter.Next(); askIter.Valid(); askIter.Next() {
+		askKey := askIter.Key()
+		askPrice := askKey[priceStartPos:priceEndPos]
+		if bytes.Compare(askPrice, firstBidPrice) > 0 {
+			break
+		} else {
+			orderIDList = append(orderIDList, string(askKey[priceEndPos:]))
+		}
+	}
+	for bidIter.Next(); bidIter.Valid(); bidIter.Next() {
+		bidKey := bidIter.Key()
+		bidPrice := bidKey[priceStartPos:priceEndPos]
+		if bytes.Compare(firstAskPrice, bidPrice) > 0 {
+			break
+		} else {
+			orderIDList = append(orderIDList, string(bidKey[priceEndPos:]))
+		}
+	}
+	result := make([]*Order, 0, len(orderIDList))
+	for _, orderID := range orderIDList {
+		result = append(result, keeper.QueryOrder(orderID))
+	}
+	return result
 }
---
+++ modules/market/orderKeeper_test.go
@@ -0,0 +1,161 @@
+package market
+
+import (
+	"fmt"
+	"bytes"
+	"github.com/cosmos/cosmos-sdk/store/transient"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"testing"
+)
+
+func bytes2str(slice []byte) string {
+	s:=""
+	for _, v := range slice {
+		s=s+fmt.Sprintf("%d ",v)
+	}
+	return s
+}
+
+func Test_concatCopyPreAllocate(t *testing.T) {
+	res := concatCopyPreAllocate([][]byte{
+		[]byte{0, 1, 2, 3},
+		[]byte{4, 5},
+		[]byte{},
+		[]byte{6, 7},
+	})
+	ref := []byte{0, 1, 2, 3, 4, 5, 6, 7}
+	if bytes.Compare(res, ref) != 0 {
+		t.Errorf("mismatch in concatCopyPreAllocate")
+	}
+}
+
+func newKeeperForTest() OrderKeeper {
+	return NewOrderKeeper(transient.NewStore(), "CET/USDT", msgCdc)
+}
+
+func simpleAddr(s string) (sdk.AccAddress, error) {
+	return sdk.AccAddressFromHex("01234567890123456789012345678901234" + s)
+}
+
+func newTO(sender string, seq uint64, price int64, qua int64, side byte, tif int, h int64) *Order {
+	addr, _ := simpleAddr(sender)
+	decPrice := sdk.NewDec(price).QuoInt(sdk.NewInt(10000))
+	return &Order{
+		Sender:      addr,
+		Sequence:    seq,
+		Symbol:      "CET/USDT",
+		OrderType:   LIMIT,
+		Price:       decPrice,
+		Quantity:    qua,
+		Side:        side,
+		TimeInForce: tif,
+		Height:      h,
+	}
+}
+
+func sameTO(a, b *Order) bool {
+	return bytes.Compare(a.Sender, b.Sender) == 0 && a.Sequence == b.Sequence &&
+		a.Symbol == b.Symbol && a.OrderType == b.OrderType && a.Price.Equal(b.Price) &&
+		a.Quantity == b.Quantity && a.Side == b.Side && a.TimeInForce == b.TimeInForce &&
+		a.Height == b.Height
+}
+
+func createTO_1() []*Order {
+	return []*Order{
+		//sender seq   price quantity       height
+		newTO("00001", 1, 11051, 50, Buy, GTE, 998), //0
+		newTO("00002", 2, 11080, 50, Buy, GTE, 998), //1 good
+		newTO("00002", 3, 10900, 50, Buy, GTE, 992), //2
+		newTO("00003", 2, 11010, 100, Sell, IOC, 997), //3 good
+		newTO("00004", 4, 11032, 60, Sell,  GTE, 990), //4
+		newTO("00005", 5, 12039, 120, Sell, GTE, 996), //5
+	}
+}
+
+func createTO_3() []*Order {
+	return []*Order{
+		//sender seq   price quantity       height
+		newTO("00001", 1, 11051, 50, Buy, GTE, 998), //0
+		newTO("00002", 2, 11080, 50, Buy, GTE, 998), //1
+		newTO("00002", 3, 10900, 50, Buy, GTE, 992), //2
+		newTO("00003", 2, 12010, 100, Sell, IOC, 997), //3
+		newTO("00004", 4, 12032, 60, Sell,  GTE, 990), //4
+		newTO("00005", 5, 12039, 120, Sell, GTE, 996), //5
+	}
+}
+
+func TestOrderBook_1(t *testing.T) {
+	orders := createTO_1()
+	keeper := newKeeperForTest()
+	for _, order := range orders {
+		keeper.Add(order)
+		fmt.Printf("0: %s %d\n",order.OrderID(),order.Height)
+	}
+	newOrder :=newTO("00005", 6, 11030, 20, Sell, GTE, 993)
+	if keeper.Remove(newOrder)==nil {
+		t.Errorf("Error in Remove")
+	}
+	orders1 := keeper.GetOlderThan(997)
+	if !( sameTO(orders1[0], orders[5]) && sameTO(orders1[1], orders[2]) && sameTO(orders1[2], orders[4]) ) {
+		t.Errorf("Error in GetOlderThan")
+	}
+	orders2 := keeper.GetOrdersAtHeight(998)
+	if !( sameTO(orders2[0], orders[0]) && sameTO(orders2[1], orders[1]) ) {
+		t.Errorf("Error in GetOlderThan")
+	}
+	addr,_ := simpleAddr("00002")
+	orderList := keeper.GetOrdersFromUser(addr.String())
+	refOrderList := []string{addr.String()+"-3",addr.String()+"-2"}
+	if orderList[0]!=refOrderList[0] || orderList[1]!=refOrderList[1] {
+		t.Errorf("Error in GetOrdersFromUser")
+	}
+	for _, order := range keeper.GetMatchingCandidates() {
+		fmt.Printf("orderID %s %s\n", order.OrderID(), order.Price.String())
+	}
+	for _, order := range orders {
+		if !keeper.Exists(order.OrderID()) {
+			t.Errorf("Can not find added orders!")
+			continue
+		}
+		qorder:=keeper.QueryOrder(order.OrderID())
+		if !sameTO(order, qorder) {
+			t.Errorf("Order's content is changed!")
+		}
+		keeper.Remove(order)
+		if keeper.Exists(order.OrderID()) {
+			t.Errorf("Can find a deleted order!")
+			continue
+		}
+	}
+}
+
+func TestOrderBook_2(t *testing.T) {
+	orders := createTO_1()
+	keeper1 := newKeeperForTest()
+	keeper2 := newKeeperForTest()
+	for _, order := range orders {
+		if order.Side==Buy {
+			keeper1.Add(order)
+		}
+		if order.Side==Sell {
+			keeper2.Add(order)
+		}
+	}
+	if len(keeper1.GetMatchingCandidates())!=0 {
+		t.Errorf("Matching result must be nil!")
+	}
+	if len(keeper2.GetMatchingCandidates())!=0 {
+		t.Errorf("Matching result must be nil!")
+	}
+}
+
+func TestOrderBook_3(t *testing.T) {
+	orders := createTO_3()
+	keeper := newKeeperForTest()
+	for _, order := range orders {
+		keeper.Add(order)
+	}
+	if len(keeper.GetMatchingCandidates())!=0 {
+		t.Errorf("Matching result must be nil!")
+	}
+}
