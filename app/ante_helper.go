68f738789e557cda782b3237ebb63dd9dadd5db0
zxh0 <zhangxiuhong2013@gmail.com>
2019-05-28 16:45:22 +0800

    Code refactor

--- app/ante_helper.go
+++ app/ante_helper.go
@@ -1,9 +1,11 @@
 package app
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+
 	"github.com/coinexchain/dex/modules/authx"
 	"github.com/coinexchain/dex/modules/bankx"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var _ authx.AnteHelper = anteHelper{}
@@ -14,11 +16,18 @@ type anteHelper struct {
 
 func (ah anteHelper) CheckMemo(msg sdk.Msg, memo string, ctx sdk.Context) sdk.Error {
 	switch msg := msg.(type) {
+	case bank.MsgSend: // should not be here!
+		return ah.checkMemo(msg.ToAddress, memo, ctx)
 	case bankx.MsgSend:
-		if ax, ok := ah.accountXKeeper.GetAccountX(ctx, msg.ToAddress); ok && ax.MemoRequired {
-			if len(memo) == 0 {
-				return bankx.ErrMemoMissing()
-			}
+		return ah.checkMemo(msg.ToAddress, memo, ctx)
+	}
+	return nil
+}
+
+func (ah anteHelper) checkMemo(addr sdk.AccAddress, memo string, ctx sdk.Context) sdk.Error {
+	if ax, ok := ah.accountXKeeper.GetAccountX(ctx, addr); ok && ax.MemoRequired {
+		if len(memo) == 0 {
+			return bankx.ErrMemoMissing()
 		}
 	}
 	return nil
