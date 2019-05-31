3b86eb3eed461c013d05af20813a7dcd3890cbf7
zxh0 <zhangxiuhong2013@gmail.com>
2019-05-31 14:26:02 +0800

    Improve ExampleGenesisCmd

--- dev/cmd_example_genesis.go
+++ dev/cmd_example_genesis.go
@@ -2,7 +2,10 @@ package dev
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
+
+	tm "github.com/tendermint/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 
@@ -15,14 +18,31 @@ func ExampleGenesisCmd(cdc *codec.Codec) *cobra.Command {
 		Short: "Print example genesis JSON",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			genState := app.NewDefaultGenesisState()
-			gsJSON, err := codec.MarshalJSONIndent(cdc, genState)
-			if err != nil {
-				return err
-			}
-			fmt.Println(string(gsJSON))
-			return nil
+			return printExampleGenesis(cdc)
 		},
 	}
 	return cmd
+}
+
+func printExampleGenesis(cdc *codec.Codec) error {
+	genState := app.NewDefaultGenesisState()
+	gneStateBytes, err := codec.MarshalJSONIndent(cdc, genState)
+	if err != nil {
+		return err
+	}
+
+	genDoc := tm.GenesisDoc{
+		ChainID:    "coinexdex",
+		Validators: nil,
+		AppState:   gneStateBytes,
+	}
+	genDoc.ValidateAndComplete()
+
+	genDocBytes, err := cdc.MarshalJSONIndent(genDoc, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(genDocBytes))
+	return nil
 }
