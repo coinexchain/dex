68f939feaf9d6b1430db77904d2c04d445c5e5a1
zxh0 <zhangxiuhong2013@gmail.com>
2019-06-03 18:37:22 +0800

    Refactor DefaultParamsCmd

--- dev/cmd_default_params.go
+++ dev/cmd_default_params.go
@@ -18,20 +18,46 @@ import (
 	"github.com/coinexchain/dex/modules/stakingx"
 )
 
+type moduleParamSet struct {
+	moduleName string
+	paramSet   params.ParamSet
+}
+
 func DefaultParamsCmd(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "default-params",
 		Short: "Print default params",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			printDefaultParams()
+			printDefaultParams(getParamSets())
 			return nil
 		},
 	}
+	cmd.Flags().Bool("include-sdk", false, "include params defined by cosmos-sdk modules")
 	return cmd
 }
 
-func printDefaultParams() {
+func getParamSets() []moduleParamSet {
+	return []moduleParamSet{
+		toParamSet("authx", authx.DefaultParams()),
+		toParamSet("bankx", bankx.DefaultParams()),
+		toParamSet("stakingx", stakingx.DefaultParams()),
+		toParamSet("asset", asset.DefaultParams()),
+		toParamSet("market", market.DefaultParams()),
+	}
+}
+
+func toParamSet(moduleName string, obj interface{}) moduleParamSet {
+	vp := reflect.New(reflect.TypeOf(obj))
+	vp.Elem().Set(reflect.ValueOf(obj))
+	vpi := vp.Interface()
+	return moduleParamSet{
+		moduleName: moduleName,
+		paramSet:   vpi.(params.ParamSet),
+	}
+}
+
+func printDefaultParams(paramSets []moduleParamSet) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Module", "Key", "Value", "Type"})
 	table.SetColumnAlignment([]int{
@@ -41,27 +67,17 @@ func printDefaultParams() {
 		tablewriter.ALIGN_LEFT,
 	})
 
-	fillParamsTable(table, "authx", authx.DefaultParams())
-	fillParamsTable(table, "bankx", bankx.DefaultParams())
-	fillParamsTable(table, "stakingx", stakingx.DefaultParams())
-	fillParamsTable(table, "asset", asset.DefaultParams())
-	fillParamsTable(table, "market", market.DefaultParams())
+	for _, paramSet := range paramSets {
+		fillParamsTable(table, paramSet.moduleName, paramSet.paramSet)
+	}
 
 	table.Render()
 }
 
-func fillParamsTable(table *tablewriter.Table, modname string, obj interface{}) {
-	ps := castToParamSet(obj)
+func fillParamsTable(table *tablewriter.Table, moduleName string, ps params.ParamSet) {
 	for _, pair := range ps.ParamSetPairs() {
 		t := reflect.Indirect(reflect.ValueOf(pair.Value)).Type().Name()
 		v := reflect.Indirect(reflect.ValueOf(pair.Value)).Interface()
-		table.Append([]string{modname, string(pair.Key), fmt.Sprintf("%v", v), t})
+		table.Append([]string{moduleName, string(pair.Key), fmt.Sprintf("%v", v), t})
 	}
-}
-
-func castToParamSet(obj interface{}) params.ParamSet {
-	vp := reflect.New(reflect.TypeOf(obj))
-	vp.Elem().Set(reflect.ValueOf(obj))
-	vpi := vp.Interface()
-	return vpi.(params.ParamSet)
 }
