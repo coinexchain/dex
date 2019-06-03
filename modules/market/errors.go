c5f83b0eae5f138aeab01140e5d18f1e4ef88cbb
zxh0 <zhangxiuhong2013@gmail.com>
2019-06-03 11:28:12 +0800

    Format code

--- modules/market/errors.go
+++ modules/market/errors.go
@@ -5,91 +5,77 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 const (
 	CodeSpaceMarket sdk.CodespaceType = "market"
 
-	CodeInvalidToken          = 120
-	CodeInvalidPricePrecision = 121
-	CodeInvalidTokenIssuer    = 122
-	CodeSendTokenFailed       = 123
-	CodeNoStoreEngine         = 124
-	CodeInvalidAddress        = 125
-	CodeNotExistKeyInStore    = 126
-	CodeNotHaveSufficientCoin = 127
-	CodeInvalidTradeSide      = 128
-	CodeInvalidOrderType      = 129
-	CodeInvalidSymbol         = 130
-	CodeTokenFrozenByIssuer   = 131
-	CodeInvalidOrderID        = 132
-	CodeMarshalFailed         = 133
-	CodeUnMarshalFailed       = 134
-	CodeNotFindOrder          = 135
-	CodeNotMatchOrderSender   = 135
+	CodeInvalidToken          sdk.CodeType = 120
+	CodeInvalidPricePrecision sdk.CodeType = 121
+	CodeInvalidTokenIssuer    sdk.CodeType = 122
+	CodeSendTokenFailed       sdk.CodeType = 123
+	CodeNoStoreEngine         sdk.CodeType = 124
+	CodeInvalidAddress        sdk.CodeType = 125
+	CodeNotExistKeyInStore    sdk.CodeType = 126
+	CodeNotHaveSufficientCoin sdk.CodeType = 127
+	CodeInvalidTradeSide      sdk.CodeType = 128
+	CodeInvalidOrderType      sdk.CodeType = 129
+	CodeInvalidSymbol         sdk.CodeType = 130
+	CodeTokenFrozenByIssuer   sdk.CodeType = 131
+	CodeInvalidOrderID        sdk.CodeType = 132
+	CodeMarshalFailed         sdk.CodeType = 133
+	CodeUnMarshalFailed       sdk.CodeType = 134
+	CodeNotFindOrder          sdk.CodeType = 135
+	CodeNotMatchOrderSender   sdk.CodeType = 135
 )
 
 func ErrTokenNoExist() sdk.Error {
-
-	return sdk.NewError(CodeSpaceMarket, CodeInvalidToken, "Token not exsit")
+	return sdk.NewError(CodeSpaceMarket, CodeInvalidToken, "Token not exist")
 }
 
 func ErrInvalidOrderID() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidOrderID, "Invalid order id")
 }
 
 func ErrInvalidPricePrecision() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidPricePrecision, "Price precision out of range")
 }
 
 func ErrInvalidPrice() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidPricePrecision, "Price out of range [0, 9E18]")
 }
 
 func ErrInvalidTokenIssuer() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidTokenIssuer, "Invalid token issuer")
 }
 
 func ErrSendTokenFailed(errStr string) sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeSendTokenFailed, "Send token failed %s", errStr)
 }
 
 func ErrNoStoreEngine() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeNoStoreEngine, "market No store engine")
 }
 
 func ErrInvalidAddress() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidAddress, "Invalid address")
 }
 
 func ErrNoExistKeyInStore() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeNotExistKeyInStore, "Not exist key in store")
 }
 
 func ErrNoHaveSufficientCoins() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeNotHaveSufficientCoin, "Not sufficient coin")
 }
 
 func ErrInvalidTradeSide() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidTradeSide, "Invalid trade side")
 }
 
 func ErrInvalidOrderType() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidOrderType, "Invalid order type")
 }
 
 func ErrInvalidSymbol() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeInvalidSymbol, "Invalid trade symbol")
 }
 
 func ErrTokenFrozenByIssuer() sdk.Error {
-
 	return sdk.NewError(CodeSpaceMarket, CodeTokenFrozenByIssuer, "Token is frozen by the issuer")
 }
