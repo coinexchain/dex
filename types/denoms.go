4abc0418d791ad0c6c3e71f8f167ac9e79520a94
zxh0 <zhangxiuhong2013@gmail.com>
2019-06-10 21:56:40 +0800

    Remove not used code

--- types/denoms.go
+++ types/denoms.go
@@ -1,23 +1,6 @@
 package types
 
-import "strings"
-
 const CET = "cet"
 
 // default bond denomination
 const DefaultBondDenom = CET
-
-// reserved token names
-var reserved = []string{
-	CET,
-	"btc", "eth", "eos",
-}
-
-func IsReserved(denom string) bool {
-	for _, d := range reserved {
-		if strings.ToLower(denom) == d {
-			return true
-		}
-	}
-	return false
-}
