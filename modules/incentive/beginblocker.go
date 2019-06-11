3a11421142fd02e07d382b02c15b6318f659ff13
Hongmin Wang <whunmr@gmail.com>
2019-06-11 19:15:06 +0800

    Check incentive_pool balance before handout rewards

--- modules/incentive/beginblocker.go
+++ modules/incentive/beginblocker.go
@@ -1,6 +1,7 @@
 package incentive
 
 import (
+	"github.com/coinexchain/dex/types"
 	"github.com/tendermint/tendermint/crypto"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,17 +11,19 @@ const BlockRewards = 50
 
 var (
 	IncentiveCoinsAccAddr = sdk.AccAddress(crypto.AddressHash([]byte("incentive_pool")))
+	BlockRewardsCoins     = sdk.NewCoins(sdk.NewInt64Coin(types.DefaultBondDenom, BlockRewards))
 )
 
 func BeginBlocker(ctx sdk.Context, k Keeper) {
 
-	//TODO: check whether there is still block incentive left
+	coins, _, err := k.bankKeeper.SubtractCoins(ctx, IncentiveCoinsAccAddr, BlockRewardsCoins)
+	if err != nil || !coins.IsValid() {
+		return
+	}
 
 	incentiveCoins := sdk.NewCoins(sdk.NewCoin("cet", sdk.NewInt(int64(BlockRewards))))
-
 	//TODO:sub the corresponding coins from genesis account
 
 	//add these coins to collectedfees
 	k.feeCollectionKeeper.AddCollectedFees(ctx, incentiveCoins)
-
 }
