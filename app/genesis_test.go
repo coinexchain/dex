a8cb4d7fe4bd7bbc3e46bd10f740ae7a69d26e17
zxh0 <zhangxiuhong2013@gmail.com>
2019-06-20 15:14:51 +0800

    Update genesis_test.go

--- app/genesis_test.go
+++ app/genesis_test.go
@@ -8,8 +8,12 @@ import (
 
 func TestDefaultGenesisState(t *testing.T) {
 	state := NewDefaultGenesisState()
-	require.Equal(t, "cet", state.StakingData.Params.BondDenom)
+	require.NoError(t, state.Validate())
+
 	require.Equal(t, "cet", state.GovData.DepositParams.MinDeposit[0].Denom)
 	require.Equal(t, "cet", state.CrisisData.ConstantFee.Denom)
+	require.Equal(t, "cet", state.StakingData.Params.BondDenom)
+	require.Equal(t, "504h0m0s", state.StakingData.Params.UnbondingTime.String()) // 21 days
+	require.Equal(t, 42, int(state.StakingData.Params.MaxValidators))
 	require.True(t, state.AuthXData.Params.MinGasPriceLimit.IsPositive())
 }
