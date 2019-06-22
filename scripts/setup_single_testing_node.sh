a4be6e84f837ffdf79dee110fea8a94615f24b9f
Hongmin Wang <whunmr@gmail.com>
2019-06-22 19:09:04 +0800

    Use GOPATH/cetd during setup_single_testing_node

--- scripts/setup_single_testing_node.sh
+++ scripts/setup_single_testing_node.sh
@@ -2,21 +2,16 @@
 
 set -e
 
-if [ ! -f "./go.mod" ]; then
-    echo "Make sure ./scripts/setup_single_testing_node.sh executed under dex code root dir."
-    exit 1
-fi
-
-if [ ! -f "./cetd" ]; then
+if [ ! -f "$GOPATH/bin/cetd" ]; then
     echo "Make sure cetd compiled by scripts/build.sh and currently can be accessed by ./cetd"
     exit 1
 fi
 
 rm -rdf ~/.cetd ~/.cetcli
-./cetd init moniker0 --chain-id=coinexdex
-./cetcli keys add bob <<<$'12345678\n12345678\n'
-./cetd add-genesis-account $(./cetcli keys show bob -a) 10000000000000000cet
-./cetd gentx --name bob <<<$'12345678\n12345678\n'
-./cetd collect-gentxs
+cetd init moniker0 --chain-id=coinexdex
+cetcli keys add bob <<<$'12345678\n12345678\n'
+cetd add-genesis-account $(cetcli keys show bob -a) 10000000000000000cet
+cetd gentx --name bob <<<$'12345678\n12345678\n'
+cetd collect-gentxs
 
 echo DONE!
