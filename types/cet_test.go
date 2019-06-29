7835c8abd08039b07d07079d8231bc1917c7cb42
jjz <woaf1003@gmail.com>
2019-06-29 17:30:13 +0800

    Add unit test

--- types/cet_test.go
+++ types/cet_test.go
@@ -1,7 +1,10 @@
 package types
 
 import (
+	"reflect"
 	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func TestNewCetCoin(t *testing.T) {
@@ -23,5 +26,66 @@ func TestNewCetCoins(t *testing.T) {
 
 	if coins[0].Amount.Int64() != 1 {
 		t.Error("coin is not 1")
+	}
+}
+
+func TestNewCetCoinE8(t *testing.T) {
+	type args struct {
+		amount int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want sdk.Coin
+	}{
+		{name: "cet", args: args{1}, want: sdk.NewInt64Coin("cet", 100000000)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewCetCoinE8(tt.args.amount); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewCetCoinE8() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewCetCoinsE8(t *testing.T) {
+	type args struct {
+		amount int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want sdk.Coins
+	}{
+		{name: "cet", args: args{1}, want: []sdk.Coin{sdk.NewInt64Coin("cet", 100000000)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewCetCoinsE8(tt.args.amount); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewCetCoinsE8() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsCET(t *testing.T) {
+	type args struct {
+		coin sdk.Coin
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "cet", args: args{sdk.NewInt64Coin("cet", 1)}, want: true},
+		{name: "btc", args: args{sdk.NewInt64Coin("btc", 1)}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsCET(tt.args.coin); got != tt.want {
+				t.Errorf("IsCET() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
