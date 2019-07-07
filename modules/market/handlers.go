cd3e4669910d80dccf765dbea6230b5d24e1d411
ludete <yyxyong@163.com>
2019-07-07 21:20:33 +0800

    Add extra amount for order

--- modules/market/handlers.go
+++ modules/market/handlers.go
@@ -17,7 +17,7 @@ const (
 	MaxTokenPricePrecision           = 18
 	LimitOrder             OrderType = 2
 	SymbolSeparator                  = "/"
-	ExtraFrozenMoney                 = 0 // 100
+	ExtraFrozenMoney                 = 100 // 100
 )
 
 type OrderType = byte
@@ -382,7 +382,7 @@ func checkMsgCancelMarket(keeper Keeper, msg MsgCancelMarket, ctx sdk.Context) s
 func calculateAmount(price, quantity int64, pricePrecision byte) sdk.Dec {
 	actualPrice := sdk.NewDec(price).Quo(sdk.NewDec(int64(math.Pow10(int(pricePrecision)))))
 	money := actualPrice.Mul(sdk.NewDec(quantity))
-	return money.Add(sdk.NewDec(ExtraFrozenMoney))
+	return money.Add(sdk.NewDec(ExtraFrozenMoney)).Ceil()
 }
 
 func marketStoreKey(prefix []byte, params ...string) []byte {
