754ac8522fa9a88d8748e53f14e483a717236261
zxh0 <zhangxiuhong2013@gmail.com>
2019-07-16 17:05:50 +0800

    Fix TestDefaultGenesisState

--- app/genesis_test.go
+++ app/genesis_test.go
@@ -5,46 +5,67 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/coinexchain/dex/modules/authx"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/crisis"
+	distr "github.com/cosmos/cosmos-sdk/x/distribution"
+	"github.com/cosmos/cosmos-sdk/x/gov"
+	"github.com/cosmos/cosmos-sdk/x/slashing"
+	"github.com/cosmos/cosmos-sdk/x/staking"
 )
 
 func TestDefaultGenesisState(t *testing.T) {
-	state := NewDefaultGenesisState()
+	state := ModuleBasics.DefaultGenesis()
 
 	//auth
-	require.Equal(t, 512, int(state.AuthData.Params.MaxMemoCharacters))
+	var authData auth.GenesisState
+	auth.ModuleCdc.MustUnmarshalJSON(state[auth.ModuleName], &authData)
+	require.Equal(t, 512, int(authData.Params.MaxMemoCharacters))
 
 	// staking
-	require.Equal(t, "cet", state.StakingData.Params.BondDenom)
-	require.Equal(t, "504h0m0s", state.StakingData.Params.UnbondingTime.String()) // 21 days
-	require.Equal(t, 42, int(state.StakingData.Params.MaxValidators))
-	require.Equal(t, 7, int(state.StakingData.Params.MaxEntries))
+	var stakingData staking.GenesisState
+	staking.ModuleCdc.MustUnmarshalJSON(state[staking.ModuleName], &stakingData)
+	require.Equal(t, "cet", stakingData.Params.BondDenom)
+	require.Equal(t, "504h0m0s", stakingData.Params.UnbondingTime.String()) // 21 days
+	require.Equal(t, 42, int(stakingData.Params.MaxValidators))
+	require.Equal(t, 7, int(stakingData.Params.MaxEntries))
 
 	// slashing
-	require.Equal(t, "504h0m0s", state.SlashingData.Params.MaxEvidenceAge.String())
-	require.Equal(t, "10m0s", state.SlashingData.Params.DowntimeJailDuration.String())
-	require.Equal(t, 10000, int(state.SlashingData.Params.SignedBlocksWindow))
-	require.Equal(t, sdk.MustNewDecFromStr("0.05"), state.SlashingData.Params.MinSignedPerWindow)
-	require.Equal(t, sdk.MustNewDecFromStr("0.05"), state.SlashingData.Params.SlashFractionDoubleSign)
-	require.Equal(t, sdk.MustNewDecFromStr("0.0001"), state.SlashingData.Params.SlashFractionDowntime)
+	var slashingData slashing.GenesisState
+	slashing.ModuleCdc.MustUnmarshalJSON(state[slashing.ModuleName], &slashingData)
+	require.Equal(t, "504h0m0s", slashingData.Params.MaxEvidenceAge.String())
+	require.Equal(t, "10m0s", slashingData.Params.DowntimeJailDuration.String())
+	require.Equal(t, 10000, int(slashingData.Params.SignedBlocksWindow))
+	require.Equal(t, sdk.MustNewDecFromStr("0.05"), slashingData.Params.MinSignedPerWindow)
+	require.Equal(t, sdk.MustNewDecFromStr("0.05"), slashingData.Params.SlashFractionDoubleSign)
+	require.Equal(t, sdk.MustNewDecFromStr("0.0001"), slashingData.Params.SlashFractionDowntime)
 
 	// distr
-	require.True(t, state.DistrData.WithdrawAddrEnabled)
-	require.Equal(t, sdk.MustNewDecFromStr("0.02"), state.DistrData.CommunityTax)
-	require.Equal(t, sdk.MustNewDecFromStr("0.01"), state.DistrData.BaseProposerReward)
-	require.Equal(t, sdk.MustNewDecFromStr("0.04"), state.DistrData.BonusProposerReward)
+	var distrData distr.GenesisState
+	distr.ModuleCdc.MustUnmarshalJSON(state[distr.ModuleName], &distrData)
+	require.True(t, distrData.WithdrawAddrEnabled)
+	require.Equal(t, sdk.MustNewDecFromStr("0.02"), distrData.CommunityTax)
+	require.Equal(t, sdk.MustNewDecFromStr("0.01"), distrData.BaseProposerReward)
+	require.Equal(t, sdk.MustNewDecFromStr("0.04"), distrData.BonusProposerReward)
 
 	// gov
-	require.Equal(t, "1000000000000cet", state.GovData.DepositParams.MinDeposit.String())
-	require.Equal(t, "336h0m0s", state.GovData.DepositParams.MaxDepositPeriod.String())
-	require.Equal(t, "336h0m0s", state.GovData.VotingParams.VotingPeriod.String())
-	require.Equal(t, sdk.MustNewDecFromStr("0.4"), state.GovData.TallyParams.Quorum)
-	require.Equal(t, sdk.MustNewDecFromStr("0.5"), state.GovData.TallyParams.Threshold)
-	require.Equal(t, sdk.MustNewDecFromStr("0.334"), state.GovData.TallyParams.Veto)
+	var govData gov.GenesisState
+	gov.ModuleCdc.MustUnmarshalJSON(state[gov.ModuleName], &govData)
+	require.Equal(t, "1000000000000cet", govData.DepositParams.MinDeposit.String())
+	require.Equal(t, "336h0m0s", govData.DepositParams.MaxDepositPeriod.String())
+	require.Equal(t, "336h0m0s", govData.VotingParams.VotingPeriod.String())
+	require.Equal(t, sdk.MustNewDecFromStr("0.4"), govData.TallyParams.Quorum)
+	require.Equal(t, sdk.MustNewDecFromStr("0.5"), govData.TallyParams.Threshold)
+	require.Equal(t, sdk.MustNewDecFromStr("0.334"), govData.TallyParams.Veto)
 
 	// crisis
-	require.Equal(t, "35000000000000cet", state.CrisisData.ConstantFee.String())
+	var crisisData crisis.GenesisState
+	crisis.ModuleCdc.MustUnmarshalJSON(state[crisis.ModuleName], &crisisData)
+	require.Equal(t, "35000000000000cet", crisisData.ConstantFee.String())
 
 	// others
-	require.Equal(t, sdk.NewDec(20), state.AuthXData.Params.MinGasPriceLimit)
+	var authxData authx.GenesisState
+	authx.ModuleCdc.MustUnmarshalJSON(state[authx.ModuleName], &authxData)
+	require.Equal(t, sdk.NewDec(20), authxData.Params.MinGasPriceLimit)
 }
