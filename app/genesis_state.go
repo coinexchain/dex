4d6616089fae248f9999a817ab5ed3217b1c8ac6
zxh0 <zhangxiuhong2013@gmail.com>
2019-07-16 16:56:31 +0800

    Reorder code

--- app/genesis_state.go
+++ app/genesis_state.go
@@ -50,30 +50,3 @@ func getAndDelete(m map[string]json.RawMessage, key string) json.RawMessage {
 	}
 	panic("key not exist: " + key)
 }
-
-//func adjustDefaultParams(gs *GenesisState) {
-//	gs.AuthData.Params.MaxMemoCharacters = DefaultMaxMemoCharacters
-
-//	gs.StakingData.Params.UnbondingTime = DefaultUnbondingTime
-//	gs.StakingData.Params.MaxValidators = DefaultMaxValidators
-//	gs.StakingData.Params.BondDenom = dex.DefaultBondDenom
-
-//	gs.SlashingData.Params.MaxEvidenceAge = DefaultMaxEvidenceAge
-//	gs.SlashingData.Params.SignedBlocksWindow = DefaultSignedBlocksWindow
-//	gs.SlashingData.Params.MinSignedPerWindow = DefaultMinSignedPerWindow
-//	gs.SlashingData.Params.SlashFractionDoubleSign = DefaultSlashFractionDoubleSign
-//	gs.SlashingData.Params.SlashFractionDowntime = DefaultSlashFractionDowntime
-
-//	gs.GovData.DepositParams.MinDeposit[0].Denom = dex.DefaultBondDenom
-//	gs.GovData.DepositParams.MinDeposit[0].Amount = DefaultGovMinDeposit
-//	gs.GovData.DepositParams.MaxDepositPeriod = DefaultPeriod
-//	gs.GovData.VotingParams.VotingPeriod = DefaultPeriod
-//	gs.GovData.TallyParams = gov.TallyParams{
-//		Quorum:    sdk.NewDecWithPrec(4, 1),
-//		Threshold: sdk.NewDecWithPrec(5, 1),
-//		Veto:      sdk.NewDecWithPrec(334, 3),
-//	}
-
-//	gs.CrisisData.ConstantFee.Denom = dex.DefaultBondDenom
-//	gs.CrisisData.ConstantFee.Amount = DefaultCrisisConstantFee
-//}
