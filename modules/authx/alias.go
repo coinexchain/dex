93df4bc3f5900b8fa258dc3db311479ce060aaa1
zxh0 <zhangxiuhong2013@gmail.com>
2019-07-17 15:16:25 +0800

    Code cleanup: types2

--- modules/authx/alias.go
+++ modules/authx/alias.go
@@ -8,4 +8,19 @@ const (
 	StoreKey     = types.StoreKey
 	QuerierRoute = types.QuerierRoute
 	ModuleName   = types.ModuleName
+
+	DefaultParamspace       = types.DefaultParamspace
+	DefaultMinGasPriceLimit = types.DefaultMinGasPriceLimit
+
+)
+
+var (
+	ErrInvalidMinGasPriceLimit = types.ErrInvalidMinGasPriceLimit
+	ErrGasPriceTooLow          = types.ErrGasPriceTooLow
+)
+
+type (
+	AccountX    = types.AccountX
+	LockedCoin  = types.LockedCoin
+	LockedCoins = types.LockedCoins
 )
