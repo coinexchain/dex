a73f96c78931ea84f5809dbb6db5a9f593c4152c
liuhaoran <lhrmachinery@163.com>
2019-07-18 14:58:01 +0800

    bankx keeper ut

--- modules/bankx/internal/keeper/keeper_test.go
+++ modules/bankx/internal/keeper/keeper_test.go
@@ -1,11 +1,15 @@
 package keeper
 
 import (
-	"fmt"
-	types3 "github.com/coinexchain/dex/modules/authx/types"
-	"github.com/coinexchain/dex/modules/bankx"
-	types2 "github.com/coinexchain/dex/modules/bankx/internal/types"
+	ax "github.com/coinexchain/dex/modules/authx/types"
+	bx "github.com/coinexchain/dex/modules/bankx/internal/types"
+	"github.com/coinexchain/dex/types"
+	"github.com/cosmos/cosmos-sdk/x/distribution"
+	"github.com/cosmos/cosmos-sdk/x/gov"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	"github.com/cosmos/cosmos-sdk/x/supply"
 	"github.com/stretchr/testify/assert"
+
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -24,7 +28,6 @@ import (
 	"github.com/coinexchain/dex/modules/authx"
 	"github.com/coinexchain/dex/modules/msgqueue"
 	"github.com/coinexchain/dex/testutil"
-	"github.com/coinexchain/dex/types"
 )
 
 type fakeAssetStatusKeeper struct{}
@@ -38,147 +41,182 @@ func (k fakeAssetStatusKeeper) IsForbiddenByTokenIssuer(ctx sdk.Context, symbol 
 
 var myaddr = testutil.ToAccAddress("myaddr")
 
-func defaultContext() (sdk.Context, params.Keeper) {
+func defaultContext() (sdk.Context, *codec.Codec, Keeper) {
 	cdc := codec.New()
+	auth.RegisterCodec(cdc)
+	supply.RegisterCodec(cdc)
+	codec.RegisterCrypto(cdc)
+	bank.RegisterCodec(cdc)
+	bx.RegisterCodec(cdc)
+
 	skey := sdk.NewKVStoreKey("test")
 	tkey := sdk.NewTransientStoreKey("transient_test")
+
+	keySupply := sdk.NewKVStoreKey("supply")
+	keyAuth := sdk.NewKVStoreKey("auth")
+	keyAuthX := sdk.NewKVStoreKey("authx")
+	keyBank := sdk.NewKVStoreKey("bank")
+	keyBankx := sdk.NewKVStoreKey("bankx")
 
 	db := dbm.NewMemDB()
 	cms := store.NewCommitMultiStore(db)
 	cms.MountStoreWithDB(skey, sdk.StoreTypeIAVL, db)
 	cms.MountStoreWithDB(tkey, sdk.StoreTypeTransient, db)
-
+	cms.MountStoreWithDB(keySupply, sdk.StoreTypeIAVL, db)
+	cms.MountStoreWithDB(keyAuth, sdk.StoreTypeIAVL, db)
+	cms.MountStoreWithDB(keyAuthX, sdk.StoreTypeIAVL, db)
+	cms.MountStoreWithDB(keyBank, sdk.StoreTypeIAVL, db)
+	cms.MountStoreWithDB(keyBankx, sdk.StoreTypeIAVL, db)
 	_ = cms.LoadLatestVersion()
+
+	maccPerms := map[string][]string{
+		auth.FeeCollectorName:     {supply.Basic},
+		distribution.ModuleName:   {supply.Basic},
+		staking.BondedPoolName:    {supply.Burner, supply.Staking},
+		staking.NotBondedPoolName: {supply.Burner, supply.Staking},
+		gov.ModuleName:            {supply.Burner},
+		authx.ModuleName:          {supply.Basic},
+		bank.ModuleName:           {supply.Basic},
+		"bankx":          {supply.Basic},
+	}
+
+	ask := fakeAssetStatusKeeper{}
+
 	ctx := sdk.NewContext(cms, abci.Header{}, false, log.NewNopLogger())
 	paramsKeeper := params.NewKeeper(cdc, skey, tkey, params.DefaultCodespace)
+	ak := auth.NewAccountKeeper(cdc, keyAuth, paramsKeeper.Subspace("auth"), auth.ProtoBaseAccount)
+	bk := bank.NewBaseKeeper(ak, paramsKeeper.Subspace("bank"), "bank")
+	sk := supply.NewKeeper(cdc, keySupply, ak, bk, supply.DefaultCodespace, maccPerms)
+	axk := authx.NewKeeper(cdc, keyAuthX, paramsKeeper.Subspace("authx"), sk, ak)
+	bxK := NewKeeper(paramsKeeper.Subspace("bankx"), axk, bk, ak, ask, sk, msgqueue.NewProducer())
 
-	return ctx, paramsKeeper
+	return ctx, cdc, bxK
 }
 
 func TestParamGetSet(t *testing.T) {
-	ctx, paramsKeeper := defaultContext()
-	subspace := paramsKeeper.Subspace(types2.DefaultParamspace)
-	bkxKepper := NewKeeper(subspace, authx.AccountXKeeper{}, bank.BaseKeeper{}, auth.AccountKeeper{}, auth.FeeCollectionKeeper{}, fakeAssetStatusKeeper{}, msgqueue.NewProducer())
+	ctx, _, keeper := defaultContext()
 
 	//expect DefaultActivationFees=1
-	defaultParam := types2.DefaultParams()
+	defaultParam := bx.DefaultParams()
 	require.Equal(t, int64(100000000), defaultParam.ActivationFee)
 
 	//expect SetParam don't panic
-	require.NotPanics(t, func() { bkxKepper.SetParam(ctx, defaultParam) }, "bankxKeeper SetParam panics")
+	require.NotPanics(t, func() { keeper.SetParam(ctx, defaultParam) }, "bankxKeeper SetParam panics")
 
 	//expect GetParam equals defaultParam
-	require.Equal(t, defaultParam, bkxKepper.GetParam(ctx))
+	require.Equal(t, defaultParam, keeper.GetParam(ctx))
 }
 
-func givenAccountWith(input bankx.testInput, addr sdk.AccAddress, coinsString string) {
+func givenAccountWith(ctx sdk.Context, keeper Keeper, addr sdk.AccAddress, coinsString string) {
 	coins, _ := sdk.ParseCoins(coinsString)
 
 	acc := auth.NewBaseAccountWithAddress(addr)
 	_ = acc.SetCoins(coins)
-	input.ak.SetAccount(bankx.ctx, &acc)
+	keeper.Ak.SetAccount(ctx, &acc)
 
-	accX := types3.AccountX{
+	accX := ax.AccountX{
 		Address: addr,
 	}
-	input.axk.SetAccountX(bankx.ctx, accX)
+	keeper.Axk.SetAccountX(ctx, accX)
 }
 
-func coinsOf(input bankx.testInput, addr sdk.AccAddress) string {
-	return input.ak.GetAccount(input.ctx, addr).GetCoins().String()
+func coinsOf(ctx sdk.Context, keeper Keeper, addr sdk.AccAddress) string {
+	return keeper.Ak.GetAccount(ctx, addr).GetCoins().String()
 }
 
-func frozenCoinsOf(input bankx.testInput, addr sdk.AccAddress) string {
-	accX, _ := input.axk.GetAccountX(bankx.ctx, addr)
+func frozenCoinsOf(ctx sdk.Context, keeper Keeper, addr sdk.AccAddress) string {
+	accX, _ := keeper.Axk.GetAccountX(ctx, addr)
 	return accX.FrozenCoins.String()
 }
 
 func TestFreezeMultiCoins(t *testing.T) {
-	input := bankx.setupTestInput()
+	ctx, _, keeper := defaultContext()
 
-	givenAccountWith(input, myaddr, "1000000000cet,100abc")
+	givenAccountWith(ctx, keeper, myaddr, "1000000000cet,100abc")
 
 	freezeCoins, _ := sdk.ParseCoins("300000000cet, 20abc")
-	err := bankx.bxk.FreezeCoins(bankx.ctx, myaddr, freezeCoins)
+	err := keeper.FreezeCoins(ctx, myaddr, freezeCoins)
 
 	require.Nil(t, err)
-	require.Equal(t, "80abc,700000000cet", coinsOf(input, myaddr))
-	require.Equal(t, "20abc,300000000cet", frozenCoinsOf(input, myaddr))
+	require.Equal(t, "80abc,700000000cet", coinsOf(ctx, keeper, myaddr))
+	require.Equal(t, "20abc,300000000cet", frozenCoinsOf(ctx, keeper, myaddr))
 
-	err = bankx.bxk.UnFreezeCoins(bankx.ctx, myaddr, freezeCoins)
+	err = keeper.UnFreezeCoins(ctx, myaddr, freezeCoins)
 
 	require.Nil(t, err)
-	require.Equal(t, "100abc,1000000000cet", coinsOf(input, myaddr))
-	require.Equal(t, "", frozenCoinsOf(input, myaddr))
+	require.Equal(t, "100abc,1000000000cet", coinsOf(ctx, keeper, myaddr))
+	require.Equal(t, "", frozenCoinsOf(ctx, keeper, myaddr))
 }
 
 func TestFreezeUnFreezeOK(t *testing.T) {
-	input := bankx.setupTestInput()
 
-	givenAccountWith(input, myaddr, "1000000000cet")
+	ctx, _, keeper := defaultContext()
+
+	givenAccountWith(ctx, keeper, myaddr, "1000000000cet")
 
 	freezeCoins := types.NewCetCoins(300000000)
-	err := bankx.bxk.FreezeCoins(bankx.ctx, myaddr, freezeCoins)
+	err := keeper.FreezeCoins(ctx, myaddr, freezeCoins)
 
 	require.Nil(t, err)
-	require.Equal(t, "700000000cet", coinsOf(input, myaddr))
-	require.Equal(t, "300000000cet", frozenCoinsOf(input, myaddr))
+	require.Equal(t, "700000000cet", coinsOf(ctx, keeper, myaddr))
+	require.Equal(t, "300000000cet", frozenCoinsOf(ctx, keeper, myaddr))
 
-	err = bankx.bxk.UnFreezeCoins(bankx.ctx, myaddr, freezeCoins)
+	err = keeper.UnFreezeCoins(ctx, myaddr, freezeCoins)
 
 	require.Nil(t, err)
-	require.Equal(t, "1000000000cet", coinsOf(input, myaddr))
-	require.Equal(t, "", frozenCoinsOf(input, myaddr))
+	require.Equal(t, "1000000000cet", coinsOf(ctx, keeper, myaddr))
+	require.Equal(t, "", frozenCoinsOf(ctx, keeper, myaddr))
 }
 
 func TestFreezeUnFreezeInvalidAccount(t *testing.T) {
-	input := bankx.setupTestInput()
+
+	ctx, _, keeper := defaultContext()
 
 	freezeCoins := types.NewCetCoins(500000000)
-	err := bankx.bxk.FreezeCoins(bankx.ctx, myaddr, freezeCoins)
+	err := keeper.FreezeCoins(ctx, myaddr, freezeCoins)
 	require.Equal(t, sdk.ErrInsufficientCoins("insufficient account funds;  < 500000000cet"), err)
 
-	err = bankx.bxk.UnFreezeCoins(bankx.ctx, myaddr, freezeCoins)
-	require.Equal(t, sdk.ErrUnknownAddress(fmt.Sprintf("account %s does not exist", myaddr)), err)
+	err = keeper.UnFreezeCoins(ctx, myaddr, freezeCoins)
+	require.Equal(t, sdk.ErrInsufficientCoins("insufficient account funds;  < 500000000cet"), err)
 }
 
 func TestFreezeUnFreezeInsufficientCoins(t *testing.T) {
-	input := bankx.setupTestInput()
+	ctx, _, keeper := defaultContext()
 
-	givenAccountWith(input, myaddr, "10cet")
+	givenAccountWith(ctx, keeper, myaddr, "10cet")
 
 	InvalidFreezeCoins := types.NewCetCoins(50)
-	err := bankx.bxk.FreezeCoins(bankx.ctx, myaddr, InvalidFreezeCoins)
+	err := keeper.FreezeCoins(ctx, myaddr, InvalidFreezeCoins)
 	require.Equal(t, sdk.ErrInsufficientCoins("insufficient account funds; 10cet < 50cet"), err)
 
 	freezeCoins := types.NewCetCoins(5)
-	err = bankx.bxk.FreezeCoins(bankx.ctx, myaddr, freezeCoins)
+	err = keeper.FreezeCoins(ctx, myaddr, freezeCoins)
 	require.Nil(t, err)
 
-	err = bankx.bxk.UnFreezeCoins(bankx.ctx, myaddr, InvalidFreezeCoins)
-	require.Equal(t, sdk.ErrInsufficientCoins("account has insufficient coins to unfreeze"), err)
+	err = keeper.UnFreezeCoins(ctx, myaddr, InvalidFreezeCoins)
+	require.Equal(t, sdk.ErrInsufficientCoins("insufficient account funds; 5cet < 50cet"), err)
 }
 
 func TestGetTotalCoins(t *testing.T) {
-	input := bankx.setupTestInput()
-	givenAccountWith(input, myaddr, "100cet, 20bch, 30btc")
+	ctx, _, keeper := defaultContext()
+	givenAccountWith(ctx, keeper, myaddr, "100cet, 20bch, 30btc")
 
-	lockedCoins := types3.LockedCoins{
-		types3.NewLockedCoin("bch", sdk.NewInt(20), 1000),
-		types3.NewLockedCoin("eth", sdk.NewInt(30), 2000),
+	lockedCoins := ax.LockedCoins{
+		ax.NewLockedCoin("bch", sdk.NewInt(20), 1000),
+		ax.NewLockedCoin("eth", sdk.NewInt(30), 2000),
 	}
 
 	frozenCoins := sdk.NewCoins(sdk.Coin{Denom: "btc", Amount: sdk.NewInt(50)},
 		sdk.Coin{Denom: "eth", Amount: sdk.NewInt(10)},
 	)
 
-	accX := types3.AccountX{
+	accX := ax.AccountX{
 		Address:     myaddr,
 		LockedCoins: lockedCoins,
 		FrozenCoins: frozenCoins,
 	}
 
-	input.axk.SetAccountX(bankx.ctx, accX)
+	keeper.Axk.SetAccountX(ctx, accX)
 
 	expected := sdk.NewCoins(
 		sdk.Coin{Denom: "bch", Amount: sdk.NewInt(40)},
@@ -187,35 +225,36 @@ func TestGetTotalCoins(t *testing.T) {
 		sdk.Coin{Denom: "eth", Amount: sdk.NewInt(40)},
 	)
 	expected = expected.Sort()
-	coins := bankx.bxk.GetTotalCoins(bankx.ctx, myaddr)
+	coins := keeper.GetTotalCoins(ctx, myaddr)
 
 	require.Equal(t, expected, coins)
 }
 
 func TestKeeper_TotalAmountOfCoin(t *testing.T) {
-	input := bankx.setupTestInput()
-	amount := bankx.bxk.TotalAmountOfCoin(bankx.ctx, "cet")
+
+	ctx, _, keeper := defaultContext()
+	amount := keeper.TotalAmountOfCoin(ctx, "cet")
 	require.Equal(t, int64(0), amount.Int64())
 
-	givenAccountWith(input, myaddr, "100cet")
+	givenAccountWith(ctx, keeper, myaddr, "100cet")
 
-	lockedCoins := types3.LockedCoins{
-		types3.NewLockedCoin("cet", sdk.NewInt(100), 1000),
+	lockedCoins := ax.LockedCoins{
+		ax.NewLockedCoin("cet", sdk.NewInt(100), 1000),
 	}
 	frozenCoins := sdk.NewCoins(sdk.Coin{Denom: "cet", Amount: sdk.NewInt(100)})
 
-	accX := types3.AccountX{
+	accX := ax.AccountX{
 		Address:     myaddr,
 		LockedCoins: lockedCoins,
 		FrozenCoins: frozenCoins,
 	}
-	input.axk.SetAccountX(bankx.ctx, accX)
-	amount = bankx.bxk.TotalAmountOfCoin(bankx.ctx, "cet")
+	keeper.Axk.SetAccountX(ctx, accX)
+	amount = keeper.TotalAmountOfCoin(ctx, "cet")
 	require.Equal(t, int64(300), amount.Int64())
 }
 
 func TestKeeper_AddCoins(t *testing.T) {
-	input := bankx.setupTestInput()
+	ctx, _, keeper := defaultContext()
 	coins := sdk.NewCoins(
 		sdk.Coin{Denom: "aaa", Amount: sdk.NewInt(10)},
 		sdk.Coin{Denom: "bbb", Amount: sdk.NewInt(20)},
@@ -226,32 +265,32 @@ func TestKeeper_AddCoins(t *testing.T) {
 		sdk.Coin{Denom: "bbb", Amount: sdk.NewInt(10)},
 	)
 
-	err := bankx.bxk.AddCoins(bankx.ctx, myaddr, coins)
+	err := keeper.AddCoins(ctx, myaddr, coins)
 	require.Equal(t, nil, err)
-	err = bankx.bxk.SubtractCoins(bankx.ctx, myaddr, coins2)
+	err = keeper.SubtractCoins(ctx, myaddr, coins2)
 	require.Equal(t, nil, err)
-	cs := bankx.bxk.GetTotalCoins(bankx.ctx, myaddr)
+	cs := keeper.GetTotalCoins(ctx, myaddr)
 	require.Equal(t, coins2, cs)
 
 	coins3 := sdk.NewCoins(
 		sdk.Coin{Denom: "aaa", Amount: sdk.NewInt(15)},
 		sdk.Coin{Denom: "bbb", Amount: sdk.NewInt(10)},
 	)
-	err = bankx.bxk.SubtractCoins(bankx.ctx, myaddr, coins3)
+	err = keeper.SubtractCoins(ctx, myaddr, coins3)
 	require.Error(t, err)
 }
 
 func TestKeeper_SendCoins(t *testing.T) {
-	input := bankx.setupTestInput()
+	ctx, _, keeper := defaultContext()
 	coins := sdk.NewCoins(
 		sdk.Coin{Denom: "aaa", Amount: sdk.NewInt(10)},
 	)
 	addr2 := testutil.ToAccAddress("addr2")
-	_ = bankx.bxk.AddCoins(bankx.ctx, myaddr, coins)
-	exist := bankx.bxk.HasCoins(bankx.ctx, myaddr, coins)
+	_ = keeper.AddCoins(ctx, myaddr, coins)
+	exist := keeper.HasCoins(ctx, myaddr, coins)
 	assert.True(t, exist)
-	err := bankx.bxk.SendCoins(bankx.ctx, myaddr, addr2, coins)
+	err := keeper.SendCoins(ctx, myaddr, addr2, coins)
 	require.Equal(t, nil, err)
-	cs := bankx.bxk.GetTotalCoins(bankx.ctx, addr2)
+	cs := keeper.GetTotalCoins(ctx, addr2)
 	require.Equal(t, coins, cs)
 }
--- modules/bankx/internal/types/msgs_test.go
+++ modules/bankx/internal/types/msgs_test.go
@@ -1,8 +1,7 @@
-package bankx
+package types
 
 import (
 	"fmt"
-	"github.com/coinexchain/dex/modules/bankx/internal/types"
 	"os"
 	"testing"
 	"time"
@@ -24,7 +23,7 @@ func TestMain(m *testing.M) {
 
 func TestSetMemoRequiredRoute(t *testing.T) {
 	addr := sdk.AccAddress([]byte("addr"))
-	msg := types.NewMsgSetTransferMemoRequired(addr, true)
+	msg := NewMsgSetTransferMemoRequired(addr, true)
 	require.Equal(t, msg.Route(), "bankx")
 	require.Equal(t, msg.Type(), "set_memo_required")
 }
@@ -34,16 +33,16 @@ func TestSetMemoRequiredValidation(t *testing.T) {
 	var emptyAddr sdk.AccAddress
 
 	testutil.ValidateBasic(t, []testutil.TestCase{
-		{Valid: true, Msg: types.NewMsgSetTransferMemoRequired(validAddr, true)},
-		{Valid: true, Msg: types.NewMsgSetTransferMemoRequired(validAddr, false)},
-		{Valid: false, Msg: types.NewMsgSetTransferMemoRequired(emptyAddr, true)},
-		{Valid: false, Msg: types.NewMsgSetTransferMemoRequired(emptyAddr, false)},
+		{Valid: true, Msg: NewMsgSetTransferMemoRequired(validAddr, true)},
+		{Valid: true, Msg: NewMsgSetTransferMemoRequired(validAddr, false)},
+		{Valid: false, Msg: NewMsgSetTransferMemoRequired(emptyAddr, true)},
+		{Valid: false, Msg: NewMsgSetTransferMemoRequired(emptyAddr, false)},
 	})
 }
 
 func TestSetMemoRequiredGetSignBytes(t *testing.T) {
 	addr := sdk.AccAddress(crypto.AddressHash([]byte("addr")))
-	msg := types.NewMsgSetTransferMemoRequired(addr, true)
+	msg := NewMsgSetTransferMemoRequired(addr, true)
 	sign := msg.GetSignBytes()
 
 	expected := `{"type":"bankx/MsgSetMemoRequired","value":{"address":"coinex15fvnexrvsm9ryw3nn4mcrnqyhvhazkkrd4aqvd","required":true}}`
@@ -52,7 +51,7 @@ func TestSetMemoRequiredGetSignBytes(t *testing.T) {
 
 func TestSetMemoRequiredGetSigners(t *testing.T) {
 	addr := sdk.AccAddress([]byte("addr"))
-	msg := types.NewMsgSetTransferMemoRequired(addr, true)
+	msg := NewMsgSetTransferMemoRequired(addr, true)
 	signers := msg.GetSigners()
 	require.Equal(t, 1, len(signers))
 	require.Equal(t, addr, signers[0])
@@ -62,7 +61,7 @@ func TestMsgSendRoute(t *testing.T) {
 	addr1 := sdk.AccAddress([]byte("from"))
 	addr2 := sdk.AccAddress([]byte("to"))
 	coins := sdk.NewCoins(sdk.NewInt64Coin("cet", 10))
-	var msg = types.NewMsgSend(addr1, addr2, coins, 10)
+	var msg = NewMsgSend(addr1, addr2, coins, 10)
 
 	require.Equal(t, msg.Route(), "bankx")
 	require.Equal(t, msg.Type(), "send")
@@ -84,18 +83,18 @@ func TestMsgSendValidation(t *testing.T) {
 
 	cases := []struct {
 		valid bool
-		tx    types.MsgSend
+		tx    MsgSend
 	}{
-		{true, types.NewMsgSend(addr1, addr2, cet123, 0)},       // valid send
-		{true, types.NewMsgSend(addr1, addr2, cet123eth123, 0)}, // valid send with multiple coins
-		{false, types.NewMsgSend(addr1, addr2, cet0, 0)},        // non positive coin
-		{false, types.NewMsgSend(addr1, addr2, cet123eth0, 0)},  // non positive coin in multicoins
-		{false, types.NewMsgSend(emptyAddr, addr2, cet123, 0)},  // empty from addr
-		{false, types.NewMsgSend(addr1, emptyAddr, cet123, 0)},  // empty to addr
-		{true, types.NewMsgSend(addr1, addr2, cet123, validTime)},
-		{false, types.NewMsgSend(addr1, addr2, cet123eth123, invalidTime)},
-		{true, types.NewMsgSend(addr1, addr2, eth123, 0)},
-		{true, types.NewMsgSend(addr1, addr2, eth123, validTime)},
+		{true, NewMsgSend(addr1, addr2, cet123, 0)},       // valid send
+		{true, NewMsgSend(addr1, addr2, cet123eth123, 0)}, // valid send with multiple coins
+		{false, NewMsgSend(addr1, addr2, cet0, 0)},        // non positive coin
+		{false, NewMsgSend(addr1, addr2, cet123eth0, 0)},  // non positive coin in multicoins
+		{false, NewMsgSend(emptyAddr, addr2, cet123, 0)},  // empty from addr
+		{false, NewMsgSend(addr1, emptyAddr, cet123, 0)},  // empty to addr
+		{true, NewMsgSend(addr1, addr2, cet123, validTime)},
+		{false, NewMsgSend(addr1, addr2, cet123eth123, invalidTime)},
+		{true, NewMsgSend(addr1, addr2, eth123, 0)},
+		{true, NewMsgSend(addr1, addr2, eth123, validTime)},
 	}
 
 	for _, tc := range cases {
@@ -112,7 +111,7 @@ func TestMsgSendGetSignBytes(t *testing.T) {
 	addr1 := sdk.AccAddress(crypto.AddressHash([]byte("input")))
 	addr2 := sdk.AccAddress(crypto.AddressHash([]byte("output")))
 	coins := sdk.NewCoins(sdk.NewInt64Coin("cet", 10))
-	var msg = types.NewMsgSend(addr1, addr2, coins, 0)
+	var msg = NewMsgSend(addr1, addr2, coins, 0)
 	res := msg.GetSignBytes()
 
 	expected := `{"type":"bankx/MsgSend","value":{"amount":[{"amount":"10","denom":"cet"}],"from_address":"coinex1e9kx6klg6z9p9ea4ehqmypl6dvjrp96vfxecd5","to_address":"coinex1urhghdgxshs9lg850mgyyqawj5lal5z460yvr8","unlock_time":"0"}}`
@@ -120,7 +119,7 @@ func TestMsgSendGetSignBytes(t *testing.T) {
 }
 
 func TestMsgSendGetSigners(t *testing.T) {
-	var msg = types.NewMsgSend(sdk.AccAddress([]byte("input1")), sdk.AccAddress{}, sdk.NewCoins(), 0)
+	var msg = NewMsgSend(sdk.AccAddress([]byte("input1")), sdk.AccAddress{}, sdk.NewCoins(), 0)
 	res := msg.GetSigners()
 	// TODO: fix this !
 	require.Equal(t, fmt.Sprintf("%v", res), "[696E70757431]")
