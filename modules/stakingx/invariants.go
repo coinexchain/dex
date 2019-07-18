656b0a3bb2f5d3629924fb87de108b322ba91083
Hongmin Wang <whunmr@gmail.com>
2019-07-18 11:36:51 +0800

    Remove unused TotalSupplyInvariants which compares with staking.Pool

--- modules/stakingx/invariants.go
+++ modules/stakingx/invariants.go
@@ -7,28 +7,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func TotalSupplyInvariants(k Keeper) sdk.Invariant {
-	return func(ctx sdk.Context) error {
-		token := k.assetViewKeeper.GetToken(ctx, types.DefaultBondDenom)
-		if token == nil {
-			return fmt.Errorf("cet not found")
-		}
-
-		//ts := token.GetTotalSupply()
-		//bondPool := k.CalcBondPoolStatus(ctx)
-
-		//TODO: compare with supplyKeeper.TotalSupply
-		//if ts != bondPool.TotalSupply.Int64() {
-		//	return fmt.Errorf("total-supply invariance:\n"+
-		//		"\tinconsistent total-supply: \n"+
-		//		"\tCET asset total supply: %v\n"+
-		//		"\tpool: %v", ts, bondPool)
-		//}
-
-		return nil
-	}
-}
-
 func SupplyCETInvariant(k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) error {
 		totalAmount := k.bk.TotalAmountOfCoin(ctx, types.CET)
