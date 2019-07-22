6783882f1948a9530782200dca33a5a9468883c8
jjz <woaf1003@gmail.com>
2019-07-22 12:31:49 +0800

    Format code

--- modules/alias/handler_test.go
+++ modules/alias/handler_test.go
@@ -1,22 +1,5 @@
 package alias
 
-import (
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"strings"
-	"testing"
-
-	"github.com/coinexchain/dex/modules/alias/internal/keepers"
-	"github.com/coinexchain/dex/modules/alias/internal/types"
-
-	sdkstore "github.com/cosmos/cosmos-sdk/store"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	abci "github.com/tendermint/tendermint/abci/types"
-	dbm "github.com/tendermint/tendermint/libs/db"
-	"github.com/tendermint/tendermint/libs/log"
-)
-
 var logStrList = make([]string, 0, 100)
 
 func logStrClear() {
