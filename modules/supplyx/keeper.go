78c03dd9da5e61bb5dd03d55779b48a36a49cbb7
zxh0 <zhangxiuhong2013@gmail.com>
2019-08-15 11:37:04 +0800

    Simplify supplyx keeper

--- modules/supplyx/keeper.go
+++ modules/supplyx/keeper.go
@@ -4,57 +4,23 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/distribution"
 	"github.com/cosmos/cosmos-sdk/x/supply"
-	supplyexported "github.com/cosmos/cosmos-sdk/x/supply/exported"
 )
 
-//wrap supply keeper to modify BurnCoins func
+// "extends" supply keeper to "override" BurnCoins() method
 type Keeper struct {
-	sk supply.Keeper
+	supply.Keeper
 	dk distribution.Keeper
 }
 
 func NewKeeper(sk supply.Keeper, dk distribution.Keeper) Keeper {
 	return Keeper{
-		sk: sk,
-		dk: dk,
+		Keeper: sk,
+		dk:     dk,
 	}
-}
-func (k Keeper) GetSupply(ctx sdk.Context) supplyexported.SupplyI {
-	return k.sk.GetSupply(ctx)
-}
-
-func (k Keeper) GetModuleAddress(name string) sdk.AccAddress {
-	return k.sk.GetModuleAddress(name)
-}
-func (k Keeper) GetModuleAccount(ctx sdk.Context, moduleName string) supplyexported.ModuleAccountI {
-	return k.sk.GetModuleAccount(ctx, moduleName)
-}
-
-func (k Keeper) SetModuleAccount(ctx sdk.Context, macc supplyexported.ModuleAccountI) {
-	k.sk.SetModuleAccount(ctx, macc)
-}
-
-func (k Keeper) SendCoinsFromModuleToModule(ctx sdk.Context, senderPool, recipientPool string, amt sdk.Coins) sdk.Error {
-	return k.sk.SendCoinsFromModuleToModule(ctx, senderPool, recipientPool, amt)
-}
-func (k Keeper) UndelegateCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) sdk.Error {
-	return k.sk.UndelegateCoinsFromModuleToAccount(ctx, senderModule, recipientAddr, amt)
-}
-func (k Keeper) DelegateCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) sdk.Error {
-	return k.sk.DelegateCoinsFromAccountToModule(ctx, senderAddr, recipientModule, amt)
-}
-
-func (k Keeper) SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string,
-	recipientAddr sdk.AccAddress, amt sdk.Coins) sdk.Error {
-	return k.sk.SendCoinsFromModuleToAccount(ctx, senderModule, recipientAddr, amt)
-}
-func (k Keeper) SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress,
-	recipientModule string, amt sdk.Coins) sdk.Error {
-	return k.sk.SendCoinsFromAccountToModule(ctx, senderAddr, recipientModule, amt)
 }
 
 func (k Keeper) BurnCoins(ctx sdk.Context, name string, amt sdk.Coins) sdk.Error {
-	err := k.sk.SendCoinsFromModuleToModule(ctx, name, distribution.ModuleName, amt)
+	err := k.Keeper.SendCoinsFromModuleToModule(ctx, name, distribution.ModuleName, amt)
 	if err != nil {
 		return err
 	}
