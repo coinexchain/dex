647ec2d5a7d92cf2b7feda620e3a99107fee13bf
selectAname <yin.jia987@gmail.com>
2019-08-19 16:12:18 +0800

    Update trust-node to be false

--- networks/test/cetdnode/rest_start.sh
+++ networks/test/cetdnode/rest_start.sh
@@ -4,4 +4,4 @@ LADDR=$(ifconfig | awk '/inet/&&!/127.0.0.1/{print substr($2,length("addr:")+1)}
 CHAIN_ID=`sed -En 's/.*chain_id": ?"([^"]*)".*/\1 /p' /cetd/node0/cetd/config/genesis.json`
 
 export _RR_TRACE_DIR="$1"
-./cetcli --home=$1 rest-server --laddr=tcp://${LADDR}:27000 --node tcp://localhost:26657 --trust-node=true --chain-id=${CHAIN_ID} > $1/rest.log
+./cetcli --home=$1 rest-server --laddr=tcp://${LADDR}:27000 --node tcp://localhost:26657 --trust-node=false --chain-id=${CHAIN_ID} > $1/rest.log
