06c5f60a3ac99cc2087a9e7af404a82c3c42429d
zxh0 <zhangxiuhong2013@gmail.com>
2019-08-20 16:18:29 +0800

    Register QueryParamsHandlerFn for bankx module

--- modules/bankx/client/rest/require_memo.go
+++ modules/bankx/client/rest/require_memo.go
@@ -1,27 +1,20 @@
 package rest
 
 import (
-	"github.com/coinexchain/dex/modules/bankx/internal/types"
 	"net/http"
-
-	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+
+	"github.com/coinexchain/dex/modules/bankx/internal/types"
 )
 
 type MemoReq struct {
 	BaseReq  rest.BaseReq `json:"base_req"`
 	Required bool         `json:"memo_required"`
-}
-
-// RegisterRoutes - Central function to define routes that get registered by the main application
-func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc("/bank/accounts/memo", SendRequestHandlerFn(cliCtx.Codec, cliCtx)).Methods("POST")
-	r.HandleFunc("/bank/accounts/{address}/transfers", SendTxRequestHandlerFn(cliCtx.Codec, cliCtx)).Methods("POST")
 }
 
 // SendRequestHandlerFn - http request handler to send coins to a address.
---
+++ modules/bankx/client/rest/rest.go
@@ -0,0 +1,14 @@
+package rest
+
+import (
+	"github.com/gorilla/mux"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+)
+
+// RegisterRoutes - Central function to define routes that get registered by the main application
+func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc("/bank/accounts/{address}/transfers", SendTxRequestHandlerFn(cliCtx.Codec, cliCtx)).Methods("POST")
+	r.HandleFunc("/bank/accounts/memo", SendRequestHandlerFn(cliCtx.Codec, cliCtx)).Methods("POST")
+	r.HandleFunc("/bankx/parameters", queryParamsHandlerFn(cliCtx)).Methods("GET")
+}
