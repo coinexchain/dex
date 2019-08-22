157798d16304b8a54386860f7114a93109bcefa1
zxh0 <zhangxiuhong2013@gmail.com>
2019-08-22 14:15:23 +0800

    Register QueryParamsHandlerFn for authx module

--- modules/authx/client/rest/query.go
+++ modules/authx/client/rest/query.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -17,6 +18,7 @@ import (
 // register REST routes
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc("/auth/accounts/{address}", QueryAccountRequestHandlerFn(cliCtx)).Methods("GET")
+	r.HandleFunc("/authx/parameters", QueryParamsHandlerFn(cliCtx)).Methods("GET")
 }
 
 // query accountREST Handler
@@ -54,5 +56,25 @@ func QueryAccountRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 		mix := types.NewAccountMix(acc, aux)
 
 		rest.PostProcessResponse(w, cliCtx, mix)
+	}
+}
+
+// HTTP request handler to query the authx params values
+func QueryParamsHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
+		if !ok {
+			return
+		}
+
+		route := fmt.Sprintf("custom/%s/%s", types.StoreKey, types.QueryParameters)
+		res, height, err := cliCtx.QueryWithData(route, nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		cliCtx = cliCtx.WithHeight(height)
+		rest.PostProcessResponse(w, cliCtx, res)
 	}
 }
