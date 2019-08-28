8d63b7a0e12a3ae74b0b0d46af7eb66a2f82720a
zxh0 <zhangxiuhong2013@gmail.com>
2019-08-28 16:44:29 +0800

    Add more unit tests

--- modules/alias/internal/keepers/querier_test.go
+++ modules/alias/internal/keepers/querier_test.go
@@ -7,9 +7,12 @@ import (
 
 	abci "github.com/tendermint/tendermint/abci/types"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/coinexchain/dex/app"
 	"github.com/coinexchain/dex/modules/alias/internal/keepers"
 	"github.com/coinexchain/dex/modules/alias/internal/types"
+	"github.com/coinexchain/dex/testutil"
 )
 
 func TestQueryParams(t *testing.T) {
@@ -18,6 +21,58 @@ func TestQueryParams(t *testing.T) {
 	testApp.AliasKeeper.SetParams(ctx, types.DefaultParams())
 
 	querier := keepers.NewQuerier(testApp.AliasKeeper)
-	_, err := querier(ctx, []string{keepers.QueryParameters}, abci.RequestQuery{})
+	res, err := querier(ctx, []string{keepers.QueryParameters}, abci.RequestQuery{})
 	require.NoError(t, err)
+
+	var params types.Params
+	testApp.Cdc.MustUnmarshalJSON(res, &params)
+	require.True(t, params.Equal(types.DefaultParams()))
+}
+
+func TestQuery(t *testing.T) {
+	testApp := app.NewTestApp()
+	ctx := testApp.NewCtx()
+
+	_, _, addr := testutil.KeyPubAddr()
+	alias := "spiderman"
+
+	testApp.AliasKeeper.SetParams(ctx, types.DefaultParams())
+	testApp.AliasKeeper.AliasKeeper.AddAlias(ctx, alias, addr, true, 10)
+
+	testQueryAddresses(t, testApp, ctx, addr, alias)
+	testQueryAliases(t, testApp, ctx, addr, alias)
+}
+
+func testQueryAddresses(t *testing.T, testApp *app.TestApp, ctx sdk.Context, addr sdk.AccAddress, alias string) {
+	reqParams := keepers.QueryAliasInfoParam{
+		QueryOp: keepers.GetAddressFromAlias,
+		Alias:   alias,
+	}
+	reqData := testApp.Cdc.MustMarshalJSON(reqParams)
+
+	querier := keepers.NewQuerier(testApp.AliasKeeper)
+	res, err := querier(ctx, []string{keepers.QueryAliasInfo}, abci.RequestQuery{Data: reqData})
+	require.NoError(t, err)
+
+	var addrs []string
+	testApp.Cdc.MustUnmarshalJSON(res, &addrs)
+	require.Equal(t, 1, len(addrs))
+	require.Equal(t, addr.String(), addrs[0])
+}
+
+func testQueryAliases(t *testing.T, testApp *app.TestApp, ctx sdk.Context, addr sdk.AccAddress, alias string) {
+	reqParams := keepers.QueryAliasInfoParam{
+		QueryOp: keepers.ListAliasOfAccount,
+		Owner:   addr,
+	}
+	reqData := testApp.Cdc.MustMarshalJSON(reqParams)
+
+	querier := keepers.NewQuerier(testApp.AliasKeeper)
+	res, err := querier(ctx, []string{keepers.QueryAliasInfo}, abci.RequestQuery{Data: reqData})
+	require.NoError(t, err)
+
+	var addrs []string
+	testApp.Cdc.MustUnmarshalJSON(res, &addrs)
+	require.Equal(t, 1, len(addrs))
+	require.Equal(t, alias, addrs[0])
 }
