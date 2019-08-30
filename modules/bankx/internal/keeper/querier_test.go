01c5ace3c364070da3207756cfa15564c851a93d
zxh0 <zhangxiuhong2013@gmail.com>
2019-08-30 18:06:01 +0800

    Create querier_test.go

---
+++ modules/bankx/internal/keeper/querier_test.go
@@ -0,0 +1,27 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/coinexchain/dex/app"
+	"github.com/coinexchain/dex/modules/bankx/internal/keeper"
+	"github.com/coinexchain/dex/modules/bankx/internal/types"
+)
+
+func Test_queryParams(t *testing.T) {
+	testApp := app.NewTestApp()
+	ctx := testApp.NewCtx()
+	params := types.DefaultParams()
+	testApp.BankxKeeper.SetParams(ctx, params)
+
+	querier := keeper.NewQuerier(testApp.BankxKeeper)
+	res, err := querier(ctx, []string{keeper.QueryParameters}, abci.RequestQuery{})
+	require.NoError(t, err)
+
+	var params2 types.Params
+	testApp.Cdc.MustUnmarshalJSON(res, &params2)
+	require.Equal(t, params, params2)
+}
