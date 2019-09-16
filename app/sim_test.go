e20519c72d84482f5fecbc60cc987cbf1ce8b0d5
zxh0 <zhangxiuhong2013@gmail.com>
2019-09-16 10:39:31 +0800

    Merge sim_utils.go and sim_test.go

--- app/sim_test.go
+++ app/sim_test.go
@@ -13,7 +13,9 @@ import (
 
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
 	"github.com/tendermint/tendermint/libs/log"
+	tmtypes "github.com/tendermint/tendermint/types"
 	dbm "github.com/tendermint/tm-db"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
@@ -49,6 +51,27 @@ import (
 	"github.com/coinexchain/dex/modules/stakingx"
 	dexsim "github.com/coinexchain/dex/simulation"
 	dex "github.com/coinexchain/dex/types"
+)
+
+var (
+	genesisFile        string
+	paramsFile         string
+	exportParamsPath   string
+	exportParamsHeight int
+	exportStatePath    string
+	exportStatsPath    string
+	seed               int64
+	initialBlockHeight int
+	numBlocks          int
+	blockSize          int
+	enabled            bool
+	verbose            bool
+	lean               bool
+	commit             bool
+	period             int
+	onOperation        bool // TODO Remove in favor of binary search for invariant violation
+	allInvariants      bool
+	genesisTime        int64
 )
 
 func init() {
@@ -932,4 +955,40 @@ func BenchmarkInvariants(b *testing.B) {
 			}
 		})
 	}
+}
+
+// AppStateFromGenesisFileFn util function to generate the genesis AppState
+// from a genesis.json file
+func AppStateFromGenesisFileFn(
+	r *rand.Rand, _ []simulation.Account, _ time.Time,
+) (json.RawMessage, []simulation.Account, string) {
+
+	var genesis tmtypes.GenesisDoc
+	cdc := MakeCodec()
+
+	bytes, err := ioutil.ReadFile(genesisFile)
+	if err != nil {
+		panic(err)
+	}
+
+	cdc.MustUnmarshalJSON(bytes, &genesis)
+
+	var appState GenesisState
+	cdc.MustUnmarshalJSON(genesis.AppState, &appState)
+
+	accounts := genaccounts.GetGenesisStateFromAppState(cdc, appState.toMap(cdc))
+
+	var newAccs []simulation.Account
+	for _, acc := range accounts {
+		// Pick a random private key, since we don't know the actual key
+		// This should be fine as it's only used for mock Tendermint validators
+		// and these keys are never actually used to sign by mock Tendermint.
+		privkeySeed := make([]byte, 15)
+		r.Read(privkeySeed)
+
+		privKey := secp256k1.GenPrivKeySecp256k1(privkeySeed)
+		newAccs = append(newAccs, simulation.Account{PrivKey: privKey, PubKey: privKey.PubKey(), Address: acc.Address})
+	}
+
+	return genesis.AppState, newAccs, genesis.ChainID
 }
