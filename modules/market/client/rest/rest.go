1daf9a4cc512cd9f2372b2fb5c6dce1e7169f756
zxh0 <zhangxiuhong2013@gmail.com>
2019-09-25 14:39:47 +0800

    Code refactor

--- modules/market/client/rest/rest.go
+++ modules/market/client/rest/rest.go
@@ -8,19 +8,23 @@ import (
 )
 
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
+	registerQueryRoutes(cliCtx, r, cdc)
 	registerTXRoutes(cliCtx, r, cdc)
+}
+
+func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
+	r.HandleFunc("/market/trading-pairs/{stock}/{money}", queryMarketHandlerFn(cdc, cliCtx)).Methods("GET")
+	r.HandleFunc("/market/exist-trading-pairs", queryMarketsHandlerFn(cdc, cliCtx)).Methods("GET")
+	r.HandleFunc("/market/orders/{order-id}", queryOrderInfoHandlerFn(cdc, cliCtx)).Methods("GET")
+	r.HandleFunc("/market/orders/account/{address}", queryUserOrderListHandlerFn(cdc, cliCtx)).Methods("GET")
+	r.HandleFunc("/market/parameters", queryParamsHandlerFn(cliCtx)).Methods("GET")
 }
 
 func registerTXRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
 	r.HandleFunc("/market/gte-orders", createGTEOrderHandlerFn(cdc, cliCtx)).Methods("POST")
 	r.HandleFunc("/market/trading-pairs", createMarketHandlerFn(cdc, cliCtx)).Methods("POST")
 	r.HandleFunc("/market/ioc-orders", createIOCOrderHandlerFn(cdc, cliCtx)).Methods("POST")
-	r.HandleFunc("/market/trading-pairs/{stock}/{money}", queryMarketHandlerFn(cdc, cliCtx)).Methods("GET")
-	r.HandleFunc("/market/exist-trading-pairs", queryMarketsHandlerFn(cdc, cliCtx)).Methods("GET")
-	r.HandleFunc("/market/orders/{order-id}", queryOrderInfoHandlerFn(cdc, cliCtx)).Methods("GET")
-	r.HandleFunc("/market/orders/account/{address}", queryUserOrderListHandlerFn(cdc, cliCtx)).Methods("GET")
 	r.HandleFunc("/market/cancel-order", cancelOrderHandlerFn(cdc, cliCtx)).Methods("POST")
 	r.HandleFunc("/market/cancel-trading-pair", cancelMarketHandlerFn(cdc, cliCtx)).Methods("POST")
 	r.HandleFunc("/market/price-precision", modifyTradingPairPricePrecision(cdc, cliCtx)).Methods("POST")
-	r.HandleFunc("/market/parameters", queryParamsHandlerFn(cliCtx)).Methods("GET")
 }
