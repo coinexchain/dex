5fd17e88a9167d5fa18964325c066a9c5dd58e55
zxh0 <zhangxiuhong2013@gmail.com>
2019-09-25 15:17:28 +0800

    Split file

---
+++ modules/market/internal/types/msgs_mq.go
@@ -0,0 +1,80 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type CreateMarketInfo struct {
+	Stock          string `json:"stock"`
+	Money          string `json:"money"`
+	PricePrecision byte   `json:"price_precision"`
+
+	// create market info
+	Creator      string `json:"creator"`
+	CreateHeight int64  `json:"create_height"`
+}
+
+type CancelMarketInfo struct {
+	Stock string `json:"stock"`
+	Money string `json:"money"`
+
+	// del market info
+	Deleter string `json:"deleter"`
+	DelTime int64  `json:"del_time"`
+}
+
+type CreateOrderInfo struct {
+	OrderID     string  `json:"order_id"`
+	Sender      string  `json:"sender"`
+	TradingPair string  `json:"trading_pair"`
+	OrderType   byte    `json:"order_type"`
+	Price       sdk.Dec `json:"price"`
+	Quantity    int64   `json:"quantity"`
+	Side        byte    `json:"side"`
+	TimeInForce int     `json:"time_in_force"`
+	FeatureFee  int64   `json:"feature_fee"`
+	Height      int64   `json:"height"`
+	FrozenFee   int64   `json:"frozen_fee"`
+	Freeze      int64   `json:"freeze"`
+}
+
+type FillOrderInfo struct {
+	OrderID     string  `json:"order_id"`
+	TradingPair string  `json:"trading_pair"`
+	Height      int64   `json:"height"`
+	Side        byte    `json:"side"`
+	Price       sdk.Dec `json:"price"`
+
+	// These fields will change when order was filled/canceled.
+	LeftStock int64 `json:"left_stock"`
+	Freeze    int64 `json:"freeze"`
+	DealStock int64 `json:"deal_stock"`
+	DealMoney int64 `json:"deal_money"`
+	CurrStock int64 `json:"curr_stock"`
+	CurrMoney int64 `json:"curr_money"`
+}
+
+type CancelOrderInfo struct {
+	OrderID     string  `json:"order_id"`
+	TradingPair string  `json:"trading_pair"`
+	Height      int64   `json:"height"`
+	Side        byte    `json:"side"`
+	Price       sdk.Dec `json:"price"`
+
+	// Del infos
+	DelReason string `json:"del_reason"`
+
+	// Fields of amount
+	UsedCommission int64 `json:"used_commission"`
+	LeftStock      int64 `json:"left_stock"`
+	RemainAmount   int64 `json:"remain_amount"`
+	DealStock      int64 `json:"deal_stock"`
+	DealMoney      int64 `json:"deal_money"`
+}
+
+type ModifyPricePrecisionInfo struct {
+	Sender            string `json:"sender"`
+	TradingPair       string `json:"trading_pair"`
+	OldPricePrecision byte   `json:"old_price_precision"`
+	NewPricePrecision byte   `json:"new_price_precision"`
+}
