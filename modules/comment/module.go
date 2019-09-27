d30912767c21f470912a92d35f662f0e95c3cba4
zxh0 <zhangxiuhong2013@gmail.com>
2019-09-27 14:09:29 +0800

    Update module.go

--- modules/comment/module.go
+++ modules/comment/module.go
@@ -3,12 +3,13 @@ package comment
 import (
 	"encoding/json"
 
-	"github.com/cosmos/cosmos-sdk/client/context"
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/coinexchain/dex/modules/comment/client/cli"
 	"github.com/coinexchain/dex/modules/comment/client/rest"
