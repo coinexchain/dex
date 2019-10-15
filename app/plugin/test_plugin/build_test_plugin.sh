71591419f6e8f222437bd59e72619ff2e97a99de
xiaoming.chen <bill_cxm@qq.com>
2019-10-15 17:35:53 +0800

    Build plugin with the same race option with unit test

--- app/plugin/test_plugin/build_test_plugin.sh
+++ app/plugin/test_plugin/build_test_plugin.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 cd `dirname $0`
-go build -race --buildmode=plugin -o ./data/plugin.so test_plugin.go
+
+RACE=$(ps -p $(ps -f -p $PPID | awk '!/PID/{print $3}') | grep ' -race ')
+
+if [[ -z ${RACE} ]]
+then
+    go build --buildmode=plugin -o ./data/plugin.so test_plugin.go
+else
+    go build -race --buildmode=plugin -o ./data/plugin.so test_plugin.go
+fi
