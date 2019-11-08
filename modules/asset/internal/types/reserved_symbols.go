96ed234fdedcf39c50316da4d5cf8ce71c43d7bd
fzc912 <fanzc912@gmail.com>
2019-11-08 10:59:15 +0800

    Update reserved_symbol

--- modules/asset/internal/types/reserved_symbols.go
+++ modules/asset/internal/types/reserved_symbols.go
@@ -6,37 +6,38 @@ var reservedSymbolMap map[string]int
 //nolint
 var reserved = []string{
 	// coin market cap currency Top 200
-	"btc", "eth", "xrp", "bch", "ltc", "usdt", "bnb", "eos", "bsv", "xlm",
-	"xmr", "ada", "leo", "ht", "trx", "dash", "etc", "xtz", "miota", "link",
-	"neo", "mkr", "usdc", "xem", "atom", "ont", "cro", "zec", "doge", "vsys",
-	"hedg", "dcr", "vet", "bat", "qtum", "pax", "btg", "tusd", "zb", "omg",
-	"lsk", "rvn", "nano", "kcs", "bcd", "inb", "algo", "waves", "btt", "nrg",
-	"hot", "icx", "theta", "bcn", "lamb", "dgb", "bts", "zrx", "hc", "npxs",
-	"rep", "iost", "aoa", "egt", "maid", "mona", "nex", "kmd", "dai", "qnt",
-	"btm", "sc", "xvg", "gnt", "rif", "ae", "zil", "etp", "steem", "snt",
-	"ardr", "ren", "mco", "wtc", "enj", "xzc", "abbc", "gxc", "snx", "solve",
-	"wax", "strat", "beam", "ela", "nexo", "elf", "grin", "zen", "pai", "eurs",
-	"ftm", "r", "wan", "fct", "etn", "ode", "new", "dent", "qash", "rdd",
-	"mana", "nuls", "nas", "dgd", "tomo", "lrc", "dgtx", "crpt", "san", "knc",
-	"matic", "loom", "wicc", "ppt", "fsn", "qkc", "lina", "eng", "aion", "fet",
-	"bix", "true", "ignis", "orbs", "ark", "one", "bnt", "bhp", "tel", "fx",
-	"powr", "xmx", "brd", "bcv", "cmt", "ttc", "celr", "c20", "valor", "tfuel",
-	"storj", "bmc", "edo", "rhoc", "iotx", "mtl", "ant", "pivx", "hyn", "abt",
-	"seele", "pzm", "fun", "agi", "gno", "divi", "rlc", "ret", "gbyte", "poly",
-	"nxt", "bft", "gas", "grs", "dac", "ugas", "rox", "nxs", "sys", "cs",
-	"cvnt", "ptt", "cos", "cvc", "ctxc", "vtc", "pla", "itc", "cbt", "gmat",
-	"noah", "tkn", "tnt", "medx", "kan", "mith", "hpb", "erd", "trio", "man",
+	"btc", "eth", "xrp", "bch", "usdt", "ltc", "eos", "bnb", "bsv", "xlm",
+	"trx", "ada", "xmr", "leo", "ht", "link", "xtz", "neo", "miota", "atom",
+	"mkr", "dash", "etc", "ont", "usdc", "cro", "xem", "bat", "doge", "vet",
+	"zec", "pax", "hedg", "qtum", "dcr", "zrx", "tusd", "hot", "btg", "cennz",
+	"vsys", "rvn", "omg", "zb", "btm", "nano", "luna", "rep", "snx", "abbc",
+	"algo", "ekt", "kcs", "dai", "btt", "lsk", "bcd", "icx", "dgb", "sc",
+	"hc", "qnt", "kmd", "waves", "bts", "bcn", "theta", "sxp", "kbc", "iost",
+	"mona", "ftt", "ae", "mco", "dx", "xvg", "maid", "nexo", "seele", "zil",
+	"nrg", "ardr", "aoa", "rlc", "chz", "enj", "steem", "rif", "elf", "snt",
+	"ren", "npxs", "gnt", "xzc", "crpt", "new", "hpt", "ilc", "solve", "ode",
+	"zen", "nex", "lamb", "gxc", "etn", "matic", "xmx", "mof", "eurs", "drg",
+	"win", "bcv", "ela", "wtc", "mana", "strat", "dgtx", "nas", "aion", "etp",
+	"brd", "grin", "beam", "pai", "nuls", "knc", "tt", "tnt", "lrc", "wicc",
+	"cvc", "true", "ppt", "fct", "ignis", "rdd", "dgd", "ftm", "fet", "ark",
+	"rcn", "gt", "wan", "fun", "tomo", "you", "ant", "r", "hbar", "eng",
+	"waxp", "iotx", "loom", "lina", "hyn", "dcn", "orbs", "edc", "bhp", "busd",
+	"qash", "man", "powr", "bnt", "c20", "dag", "dent", "storj", "mtl", "nxs",
+	"uni", "san", "gno", "kan", "cocos", "rox", "stream", "grs", "abt", "bix",
+	"cs", "cmt", "edo", "one", "tel", "gas", "fx", "medx", "adn", "celr",
+	"sys", "wxt", "erd", "wgr", "lend", "mda", "dpt", "loki", "iris", "egt",
 
-	// coinex exchange currency: 90
-	"ont", "grin", "gnt", "atom", "qtum", "eth", "btc", "ae", "wings", "btm",
-	"dcr", "olt", "neo", "icx", "bat", "etc", "kmd", "xzc", "dash", "iota",
-	"btt", "vet", "okb", "stx", "algo", "link", "cnn", "tusd", "lsk", "ltc",
-	"omg", "zil", "xrp", "zec", "hot", "bch", "ada", "cody", "nano", "zrx",
-	"hydro", "trx", "cet", "seele", "doge", "dot", "ftt", "bsv", "hc", "btu",
-	"xmr", "dero", "eos", "xtz", "spice", "xvg", "ong", "rvn", "sys", "usdh",
-	"loom", "xem", "dgb", "bnb", "nnb", "xlm", "waves", "ult", "usdt", "usdc",
-	"lamb", "wwb", "kan", "ckb", "ht", "pax", "ctxc", "gusd", "gram", "cmt",
-	"trtl", "vtho", "akro", "whc", "sc", "lfc", "tct", "gas", "egt", "fcny",
+	// coinex exchange currency: 96
+	"ont", "grin", "gnt", "atom", "qtum", "eth", "btc", "omg", "ae", "wings",
+	"btm", "eosc", "dcr", "loom", "olt", "neo", "icx", "bat", "etc", "kmd",
+	"xzc", "dash", "iota", "btt", "vet", "okb", "xtz", "link", "stx", "algo",
+	"hot", "cnn", "tusd", "lsk", "ltc", "zil", "xrp", "zec", "vsys", "bch",
+	"ada", "cmt", "cody", "nano", "zrx", "hydro", "trx", "cet", "seele", "doge",
+	"dot", "ftt", "bsv", "hc", "btu", "iost", "sai", "xmr", "dero", "eos",
+	"spice", "xvg", "spok", "ong", "rvn", "sys", "usdh", "xem", "dgb", "bnb",
+	"nnb", "xlm", "waves", "ela", "ardr", "ult", "usdt", "usdc", "lamb", "sc",
+	"kan", "ckb", "ht", "pax", "ctxc", "gusd", "gram", "trtl", "vtho", "akro",
+	"whc", "lfc", "tct", "gas", "egt", "fcny",
 
 	// ISO 8601 fiat currency: 93
 	"usd", "all", "dzd", "ars", "amd", "aud", "azn", "bhd", "bdt", "byn",
@@ -54,7 +55,7 @@ var reserved = []string{
 	"xau", "xag", "xpt", "xpd",
 
 	// extra currency
-	"libra", "cet", "rmb",
+	"libra","cet","rmb","dcep","coinex","viabtc","matrix","bitmain",
 }
 
 func init() {
