3c2c0edf6ca45154f15253bc64924a470636b843
xiaoming.chen <bill_cxm@qq.com>
2019-11-21 18:01:10 +0800

    Check if memo empty for MsgSupervisedSend

--- app/ante_helper.go
+++ app/ante_helper.go
@@ -32,6 +32,8 @@ func (ah anteHelper) CheckMsg(ctx sdk.Context, msg sdk.Msg, memo string) sdk.Err
 	switch msg := msg.(type) {
 	case bankx.MsgSend:
 		return ah.checkMemo(ctx, msg.ToAddress, memo)
+	case bankx.MsgSupervisedSend:
+		return ah.checkMemo(ctx, msg.ToAddress, memo)
 	case bankx.MsgMultiSend:
 		for _, out := range msg.Outputs {
 			if err := ah.checkMemo(ctx, out.Address, memo); err != nil {
