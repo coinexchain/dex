c9811d6e6a6386a88351f4478fe9c7dd3d6f7ec3
zxh0 <zhangxiuhong2013@gmail.com>
2019-11-27 09:59:18 +0800

    Update mykey.sh

--- scripts/mykey.sh
+++ scripts/mykey.sh
@@ -5,16 +5,16 @@ if ! type $CLI > /dev/null; then
   exit 0
 fi
 if [ -z "$1" ]; then
-  echo 'Usage: sh mykey.sh <postfix>'
+  echo 'Usage: sh mykey.sh <suffix>'
   exit 0
 fi
 
-printf "finding address with postfix '%s' ...\n" $1
+printf "finding address with suffix '%s' ...\n" $1
 for i in {1..50000}
 do
   OUTPUT=`$CLI keys add xxx --dry-run 2>&1`
   ADDR=`echo $OUTPUT | grep -o 'address: [a-z0-9]*' | grep -o 'coinex[0-9a-z]*'`
-  printf "%d\t%s\r" $i $ADDR
+  printf "\t%d\t%s\r" $i $ADDR
   if [[ $ADDR == *$1 ]]; then
     echo ok $ADDR
     echo $OUTPUT
