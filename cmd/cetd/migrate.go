a016372eb1972dd7e9cc1a7fbd029af266f898e3
zxh0 <zhangxiuhong2013@gmail.com>
2019-12-04 14:41:02 +0800

    Implement CIP0004
    Changelog, swagger & migration tool

---
+++ cmd/cetd/migrate.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	tm "github.com/tendermint/tendermint/types"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	"github.com/coinexchain/dex/app"
+	"github.com/coinexchain/dex/modules/asset"
+)
+
+const (
+	flagOutput = "output"
+)
+
+func migrateCmd(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "migrate [from]",
+		Short: "Migrate genesis.json (coinexdex -> coinexdex2)",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			inputFile := args[0]
+			outputFile := viper.GetString(flagOutput)
+			return migrateGenesisFile(cdc, inputFile, outputFile)
+		},
+	}
+
+	cmd.Flags().String(flagOutput, "", "New genesis.json file")
+	return cmd
+}
+
+func migrateGenesisFile(cdc *codec.Codec, inputFile, outputFile string) error {
+	data, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	genDoc := &tm.GenesisDoc{}
+	cdc.MustUnmarshalJSON(data, genDoc)
+
+	genState := &app.GenesisState{}
+	cdc.MustUnmarshalJSON(genDoc.AppState, genState)
+
+	upgradeGenesisState(genState)
+
+	genDoc.AppState = cdc.MustMarshalJSON(genState)
+	data = cdc.MustMarshalJSON(genDoc)
+
+	if outputFile == "" {
+		fmt.Println(data)
+		return nil
+	}
+	return ioutil.WriteFile(outputFile, data, 0644)
+}
+
+func upgradeGenesisState(genState *app.GenesisState) {
+	genState.AssetData.Params = asset.DefaultParams()
+	// TODO: more upgrades
+}
