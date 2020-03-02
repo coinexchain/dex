39c3f513935e6305561762567f680f37512c00d8
liuhaoran <lhrmachinery@163.com>
2020-03-02 11:21:58 +0800

    Add upgrade script

---
+++ scripts/4_upgrade_chain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eux
+
+if [ ! -f "./cetd" ]; then
+  echo "Make sure cetd currently can be find in pwd"
+  exit 1
+fi
+
+if [ ! -f "./cetcli" ]; then
+  echo "Make sure cetcli currently can be find in pwd"
+  exit 1
+fi
+
+GENESIS_FILE=genesis.json
+
+cetd export --for-zero-height=false > ${GENESIS_FILE}
+cetd migrate ${GENESIS_FILE} --genesis-block-height="${GENESIS_BLOCK_HEIGHT:-0}" --output ${GENESIS_FILE}
+cetd unsafe-reset-all
+cp ${GENESIS_FILE} "${CHAIN_DIR:-${HOME}/.cetd}"/config/genesis.json
+
+rm -rf ${GENESIS_FILE}
+
+echo Done!
