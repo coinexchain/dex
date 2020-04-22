f17ee485a475ca8674547932cc4816b8100c5220
zxh0 <zhangxiuhong2013@gmail.com>
2020-04-22 17:11:12 +0800

    Add CreateContextAndRegisterRoutes()

--- app/utils.go
+++ app/utils.go
@@ -4,12 +4,17 @@ import (
 	"os"
 	"strings"
 
-	"github.com/coinexchain/cet-sdk/msgqueue"
-
-	cfg "github.com/tendermint/tendermint/config"
-
+	"github.com/gorilla/mux"
 	toml "github.com/pelletier/go-toml"
 	"github.com/spf13/viper"
+	cfg "github.com/tendermint/tendermint/config"
+	rpcclient "github.com/tendermint/tendermint/rpc/client"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	authrest "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
+
+	"github.com/coinexchain/cet-sdk/msgqueue"
 )
 
 const (
@@ -48,4 +53,44 @@ func getPreFixBks(prefix string) string {
 		}
 	}
 	return ""
+}
+
+func CreateContextAndRegisterRoutes(router *mux.Router) {
+	var ctx = newCLIContextForEmbeddedLDC()
+	client.RegisterRoutes(ctx, router)
+	authrest.RegisterTxRoutes(ctx, router)
+	ModuleBasics.RegisterRESTRoutes(ctx, router)
+}
+
+// see cosmos-sdk/client/context/context.go#NewCLIContextWithFrom()
+func newCLIContextForEmbeddedLDC() context.CLIContext {
+	var cdc = MakeCodec()
+	var nodeURI = viper.GetString("address")
+	if nodeURI == "" {
+		nodeURI = "tcp://localhost:26657"
+	}
+	var rpc = rpcclient.NewHTTP(nodeURI, "/websocket")
+
+	// fill members of ctx
+	return context.CLIContext{
+		Codec:   cdc,
+		Client:  rpc,
+		NodeURI: nodeURI,
+		TrustNode:     true,
+
+		// default values is enough?
+		//Output:        os.Stdout,
+		//From:          viper.GetString(flags.FlagFrom),
+		//OutputFormat:  viper.GetString(cli.OutputFlag),
+		//Height:        viper.GetInt64(flags.FlagHeight),
+		//UseLedger:     viper.GetBool(flags.FlagUseLedger),
+		//BroadcastMode: viper.GetString(flags.FlagBroadcastMode),
+		//Verifier:      verifier,
+		//Simulate:      viper.GetBool(flags.FlagDryRun),
+		//GenerateOnly:  genOnly,
+		//FromAddress:   fromAddress,
+		//FromName:      fromName,
+		//Indent:        viper.GetBool(flags.FlagIndentResponse),
+		//SkipConfirm:   viper.GetBool(flags.FlagSkipConfirmation),
+	}
 }
